7d2dc27333a9bfa2a8cf4ee68ddd0d97ba2fa505
Marc <marc@lodgify.com>
2018-03-13 15:41:51 +0100

    Make consequent changes to other input elements and InputController

--- src/components/elements/InputController/component.spec.js
+++ src/components/elements/InputController/component.spec.js
@@ -4,26 +4,30 @@ import { shallow, mount } from 'enzyme';
 import { Component as InputController } from './component';
 
 const props = {
-  tagName: 'input',
   error: false,
   isValid: false,
-  label: '',
   name: 'someName',
   onChange: Function.prototype,
 };
+const children = <input />;
+
+const getInputController = extraProps =>
+  shallow(
+    <InputController {...props} {...extraProps}>
+      {children}
+    </InputController>
+  );
 
 describe('<InputController />', () => {
   describe('default behaviour', () => {
     it('should render a single Semantic UI `Input` component', () => {
-      const inputController = shallow(<InputController {...props} />);
+      const inputController = getInputController();
       const actual = inputController.find('Input').length;
       expect(actual).toBe(1);
     });
 
     it('should default to no classNames on `Input`', () => {
-      const semanticInput = shallow(<InputController {...props} />).find(
-        'Input'
-      );
+      const semanticInput = getInputController().find('Input');
       const classNames = ['dirty', 'valid', 'error'];
       classNames.forEach(className => {
         const actual = semanticInput.hasClass(className);
@@ -31,48 +35,43 @@ describe('<InputController />', () => {
       });
     });
 
-    it('should pass a function as `props.onChange` to `input`', () => {
-      const htmlInput = shallow(<InputController {...props} />).find('input');
-      const actual = htmlInput.prop('onChange');
-      expect(actual).toEqual(expect.any(Function));
-    });
-
-    it('should render a single html `input` inside `Input`', () => {
-      const semanticInput = shallow(<InputController {...props} />).find(
-        'Input'
-      );
+    it('should render the children inside `Input`', () => {
+      const semanticInput = getInputController().find('Input');
       const actual = semanticInput.find('input').length;
       expect(actual).toBe(1);
     });
 
-    it('should render a single html `textarea` inside `Input`', () => {
-      const semanticInput = shallow(
-        <InputController {...props} tagName="textarea" />
-      ).find('Input');
-      const actual = semanticInput.find('textarea').length;
-      expect(actual).toBe(1);
+    describe('the children', () => {
+      it('should get a function as `props.onChange` by default', () => {
+        const htmlInput = getInputController().find('input');
+        const actual = htmlInput.prop('onChange');
+        expect(actual).toEqual(expect.any(Function));
+      });
+
+      it('should get a function as `props[inputOnChangeFunctionName]` if specified', () => {
+        const inputOnChangeFunctionName = 'whoopDeDooo';
+        const htmlInput = getInputController({
+          inputOnChangeFunctionName,
+        }).find('input');
+        const actual = htmlInput.prop(inputOnChangeFunctionName);
+        expect(actual).toEqual(expect.any(Function));
+      });
     });
 
     it('should not render the `ErrorMessage` component', () => {
-      const semanticInput = shallow(<InputController {...props} />).find(
-        'Input'
-      );
+      const semanticInput = getInputController().find('Input');
       const actual = semanticInput.find('ErrorMessage').length;
       expect(actual).toBe(0);
     });
 
     it('should not render the `Icon` component', () => {
-      const semanticInput = shallow(<InputController {...props} />).find(
-        'Input'
-      );
+      const semanticInput = getInputController().find('Input');
       const actual = semanticInput.find('Icon').length;
       expect(actual).toBe(0);
     });
 
     it('should not render the `label` element', () => {
-      const semanticInput = shallow(<InputController {...props} />).find(
-        'Input'
-      );
+      const semanticInput = getInputController().find('Input');
       const actual = semanticInput.find('Icon').length;
       expect(actual).toBe(0);
     });
@@ -87,9 +86,9 @@ describe('<InputController />', () => {
     it('should add `className` `error` to `Input`', () => {
       const errorValues = [true, "Something's not right"];
       errorValues.forEach(errorValue => {
-        const semanticInput = shallow(
-          <InputController {...props} error={errorValue} />
-        ).find('Input');
+        const semanticInput = getInputController({ error: errorValue }).find(
+          'Input'
+        );
         const actual = semanticInput.hasClass('error');
         expect(actual).toBe(true);
       });
@@ -97,7 +96,7 @@ describe('<InputController />', () => {
 
     describe('if `props.error` is boolean', () => {
       it('should not render the `ErrorMessage` component', () => {
-        const inputController = shallow(<InputController {...props} error />);
+        const inputController = getInputController({ error: true });
         const actual = inputController.find('ErrorMessage').length;
         expect(actual).toBe(0);
       });
@@ -105,26 +104,20 @@ describe('<InputController />', () => {
 
     describe('if `props.error` is a string', () => {
       it('should treat an empty string as false', () => {
-        const semanticInput = shallow(
-          <InputController {...props} error="" />
-        ).find('Input');
+        const semanticInput = getInputController({ error: '' }).find('Input');
         const actual = semanticInput.hasClass('error');
         expect(actual).toBe(false);
       });
 
       it('should render a single `ErrorMessage` component', () => {
-        const inputController = shallow(
-          <InputController {...props} error="Oops" />
-        );
+        const inputController = getInputController({ error: 'Oops' });
         const actual = inputController.find('ErrorMessage').length;
         expect(actual).toBe(1);
       });
 
       it('should pass `props.error` to the `ErrorMessage` component', () => {
         const errorMessage = 'Oh nooes';
-        const inputController = shallow(
-          <InputController {...props} error={errorMessage} />
-        );
+        const inputController = getInputController({ error: errorMessage });
         const actual = inputController
           .find('ErrorMessage')
           .prop('errorMessage');
@@ -135,21 +128,19 @@ describe('<InputController />', () => {
 
   describe('State: valid', () => {
     it('should add `className` `valid` to `Input`', () => {
-      const semanticInput = shallow(
-        <InputController {...props} isValid />
-      ).find('Input');
+      const semanticInput = getInputController({ isValid: true }).find('Input');
       const actual = semanticInput.hasClass('valid');
       expect(actual).toBe(true);
     });
 
     it('should render a single `Icon` component', () => {
-      const inputController = shallow(<InputController {...props} isValid />);
+      const inputController = getInputController({ isValid: true });
       const actual = inputController.find('Icon').length;
       expect(actual).toBe(1);
     });
 
     it('should pass the `Icon` component the right props', () => {
-      const inputController = shallow(<InputController {...props} isValid />);
+      const inputController = getInputController({ isValid: true });
       const actual = inputController.find('Icon').props();
       expect(actual).toEqual(
         expect.objectContaining({
@@ -161,28 +152,32 @@ describe('<InputController />', () => {
     });
   });
 
+  describe('State: focused', () => {
+    it('should add `className` `focus` to `Input`', () => {
+      const semanticInput = getInputController({ isFocused: true }).find(
+        'Input'
+      );
+      const actual = semanticInput.hasClass('focus');
+      expect(actual).toBe(true);
+    });
+  });
+
   describe('Variation: labelled', () => {
     it('should render a single html `label` inside `Input`', () => {
-      const semanticInput = shallow(
-        <InputController {...props} label="yo" />
-      ).find('Input');
+      const semanticInput = getInputController({ label: 'yo' }).find('Input');
       const actual = semanticInput.find('label').length;
       expect(actual).toBe(1);
     });
 
     it('should pass a function as `props.onClick` to `label`', () => {
-      const semanticInput = shallow(
-        <InputController {...props} label="yo" />
-      ).find('Input');
+      const semanticInput = getInputController({ label: 'yo' }).find('Input');
       const label = semanticInput.find('label');
       const actual = label.prop('onClick');
       expect(actual).toEqual(expect.any(Function));
     });
 
     it('should render the `props.label`', () => {
-      const semanticInput = shallow(
-        <InputController {...props} label="yo" />
-      ).find('Input');
+      const semanticInput = getInputController({ label: 'yo' }).find('Input');
       const label = semanticInput.find('label');
       const actual = label.contains('yo');
       expect(actual).toBe(true);
@@ -191,12 +186,12 @@ describe('<InputController />', () => {
 
   describe('Interaction: onChange', () => {
     it('should persist the value in component state', () => {
-      const event = { target: { value: '🐸' } };
-      const inputController = shallow(<InputController {...props} />);
+      const value = '🐸';
+      const inputController = getInputController();
       const htmlInput = inputController.find('input');
-      htmlInput.simulate('change', event);
+      htmlInput.simulate('change', value);
       const actual = inputController.state('value');
-      expect(actual).toBe(event.target.value);
+      expect(actual).toBe(value);
     });
   });
 
@@ -204,9 +199,7 @@ describe('<InputController />', () => {
     it('should call the function passed as `props.onChange`', () => {
       const value = 'someValue';
       const handleChange = jest.fn();
-      const inputController = shallow(
-        <InputController {...props} onChange={handleChange} />
-      );
+      const inputController = getInputController({ onChange: handleChange });
       inputController.setState({ value });
       expect(handleChange).toHaveBeenCalledWith(props.name, value);
     });
@@ -215,7 +208,9 @@ describe('<InputController />', () => {
   describe('Interaction: onClick dynamic `label`', () => {
     it('should force focus on the html `input`', () => {
       const inputController = mount(
-        <InputController {...props} label="someLabel" />
+        <InputController {...props} label="someLabel">
+          {children}
+        </InputController>
       );
       const htmlInput = inputController.find('input').instance();
       const htmlLabel = inputController.find('label');
--- src/components/elements/TextInput/component.spec.js
+++ src/components/elements/TextInput/component.spec.js
@@ -3,22 +3,41 @@ import { shallow } from 'enzyme';
 
 import { Component as TextInput } from './component';
 
+const getTextInput = () => shallow(<TextInput />);
+
 describe('<TextInput />', () => {
   it('should render a single `InputController` component', () => {
-    const textInput = shallow(<TextInput />);
+    const textInput = getTextInput();
     const actual = textInput.find('InputController').length;
     expect(actual).toBe(1);
   });
 
   it('should pass the right `props` to `InputController`', () => {
-    const inputController = shallow(<TextInput />).find('InputController');
+    const inputController = getTextInput().find('InputController');
     const actual = inputController.props();
     expect(actual).toEqual(
       expect.objectContaining({
-        tagName: 'input',
-        ...TextInput.defaultProps,
+        error: false,
+        isValid: false,
+        label: '',
+        name: '',
+        onChange: Function.prototype,
       })
     );
+  });
+
+  it('should pass an `input` as a child to `InputController`', () => {
+    const inputController = getTextInput().find('InputController');
+    const actual = inputController.children('input');
+    expect(actual).toHaveLength(1);
+  });
+
+  describe('the child `input`', () => {
+    it('should get the `TextInput.props.type` as `props.type`', () => {
+      const htmlInput = getTextInput().find('input');
+      const actual = htmlInput.prop('type');
+      expect(actual).toBe('text');
+    });
   });
 
   it('should have displayName `TextInput`', () => {
