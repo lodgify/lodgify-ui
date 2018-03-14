b933fd6064c016b67af0f53caab6663972fe7789
Marc <marc@lodgify.com>
2018-03-14 13:11:04 +0100

    Add tests and examples.

--- src/components/elements/CaptchaInput/Readme.md
+++ src/components/elements/CaptchaInput/Readme.md
@@ -5,3 +5,40 @@ const mockCaptcha = require('./mock-data/signupcode.jpeg');
   <CaptchaInput image={mockCaptcha} />
 </Form>
 ```
+
+### States
+
+#### Error
+
+```jsx
+const mockCaptcha = require('./mock-data/signupcode.jpeg');
+
+<Form>
+  <CaptchaInput error image={mockCaptcha} />
+  <Divider />
+  <Divider />
+  <CaptchaInput error="Something's not right" image={mockCaptcha} />
+</Form>
+```
+
+#### Valid
+
+```jsx
+const mockCaptcha = require('./mock-data/signupcode.jpeg');
+
+<Form>
+  <CaptchaInput isValid image={mockCaptcha} />
+</Form>
+```
+
+### Variations
+
+#### Labeled
+
+```jsx
+const mockCaptcha = require('./mock-data/signupcode.jpeg');
+
+<Form>
+  <CaptchaInput label="Security code" image={mockCaptcha} />
+</Form>
+```
--- src/components/elements/CaptchaInput/component.spec.js
+++ src/components/elements/CaptchaInput/component.spec.js
@@ -1,47 +1,89 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Form, Image } from 'semantic-ui-react';
 
-import { Component as TextInput } from './component';
+import { TextInput } from 'elements/TextInput';
 
-const getTextInput = () => shallow(<TextInput />);
+import { Component as CaptchaInput } from './component';
 
-describe('<TextInput />', () => {
-  it('should render a single `InputController` component', () => {
-    const textInput = getTextInput();
-    const actual = textInput.find('InputController').length;
-    expect(actual).toBe(1);
-  });
+const image = 'someUrl';
+const getCaptchaInput = () => shallow(<CaptchaInput image={image} />);
+const getFormGroup = () => getCaptchaInput().find(Form.Group);
 
-  it('should pass the right `props` to `InputController`', () => {
-    const inputController = getTextInput().find('InputController');
-    const actual = inputController.props();
-    expect(actual).toEqual(
-      expect.objectContaining({
-        error: false,
-        isValid: false,
-        label: '',
-        name: '',
-        onChange: Function.prototype,
-      })
-    );
-  });
-
-  it('should pass an `input` as a child to `InputController`', () => {
-    const inputController = getTextInput().find('InputController');
-    const actual = inputController.children('input');
+describe('<CaptchaInput />', () => {
+  it('should render a single Semantic UI `Form.Group` component', () => {
+    const actual = getFormGroup();
     expect(actual).toHaveLength(1);
   });
 
-  describe('the child `input`', () => {
-    it('should get the `TextInput.props.type` as `props.type`', () => {
-      const htmlInput = getTextInput().find('input');
-      const actual = htmlInput.prop('type');
-      expect(actual).toBe('text');
+  describe('the `Form.Group` component', () => {
+    it('should get the right props', () => {
+      const formGroup = getFormGroup();
+      const actual = formGroup.props();
+      expect(actual).toEqual(
+        expect.objectContaining({
+          widths: 'equal',
+        })
+      );
+    });
+
+    it('should render two children', () => {
+      const formGroup = getFormGroup();
+      const actual = formGroup.children();
+      expect(actual).toHaveLength(2);
+    });
+
+    it('should render one Semantic UI `Form.Field` component', () => {
+      const formGroup = getFormGroup();
+      const actual = formGroup.children(Form.Field);
+      expect(actual).toHaveLength(1);
+    });
+
+    it('should render one Lodgify UI `TextInput` component', () => {
+      const formGroup = getFormGroup();
+      const actual = formGroup.children(TextInput);
+      expect(actual).toHaveLength(1);
     });
   });
 
-  it('should have displayName `TextInput`', () => {
-    const actual = TextInput.displayName;
-    expect(actual).toBe('TextInput');
+  describe('the `Form.Field` component', () => {
+    it('should render one Semantic UI `Image` component', () => {
+      const formField = getCaptchaInput().find(Form.Field);
+      const actual = formField.children(Image);
+      expect(actual).toHaveLength(1);
+    });
+  });
+
+  describe('the `Image` component', () => {
+    it('should get the right props', () => {
+      const imageComponent = getCaptchaInput().find(Image);
+      const actual = imageComponent.props();
+      expect(actual).toEqual(
+        expect.objectContaining({
+          src: image,
+        })
+      );
+    });
+  });
+
+  describe('the `TextInput` component', () => {
+    it('should get the right props', () => {
+      const textInput = getCaptchaInput().find(TextInput);
+      const actual = textInput.props();
+      expect(actual).toEqual(
+        expect.objectContaining({
+          error: false,
+          isValid: false,
+          label: '',
+          name: '',
+          onChange: Function.prototype,
+        })
+      );
+    });
+  });
+
+  it('should have displayName `CaptchaInput`', () => {
+    const actual = CaptchaInput.displayName;
+    expect(actual).toBe('CaptchaInput');
   });
 });
