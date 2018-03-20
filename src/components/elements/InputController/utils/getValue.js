dc210d17a3c17008c7a1346364e5add9f920c0a6
Marc <marc@lodgify.com>
2018-03-20 13:11:48 +0100

    Fix getValue to respect defined falsy values

--- src/components/elements/InputController/utils/getValue.js
+++ src/components/elements/InputController/utils/getValue.js
@@ -4,5 +4,7 @@ import { get } from 'lodash';
  * @param  {Object|any} eventOrValue
  * @return {any}
  */
-export const getValue = eventOrValue =>
-  get(eventOrValue, ['target', 'value']) || eventOrValue;
+export const getValue = eventOrValue => {
+  const nestedValue = get(eventOrValue, ['target', 'value']);
+  return nestedValue !== undefined ? nestedValue : eventOrValue;
+};
