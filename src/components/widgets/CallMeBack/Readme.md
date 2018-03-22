26d6bd36a8616987ff87500a600f6ce5416a3c7c
Marc <marc@lodgify.com>
2018-03-22 14:58:44 +0100

    Add component and example

---
+++ src/components/widgets/CallMeBack/Readme.md
@@ -0,0 +1,15 @@
+```jsx
+const {
+  dateOptions,
+  propertyOptions,
+  timeOptions,
+  timeZoneOptions,
+} = require('./mock-data/options');
+
+<CallMeBack
+  dateOptions={dateOptions}
+  propertyOptions={propertyOptions}
+  timeOptions={timeOptions}
+  timeZoneOptions={timeZoneOptions}
+/>
+```
---
+++ src/components/widgets/CallMeBack/mock-data/options.js
@@ -0,0 +1,38 @@
+export const dateOptions = [
+  { text: 'Today', value: 'today' },
+  { text: 'Tomorrow', value: 'tomorrow' },
+];
+
+export const propertyOptions = [
+  {
+    text: 'La Casa Viva',
+    value: 'casaViva',
+  },
+  {
+    text: 'La Casa Muerta',
+    value: 'casaMuerta',
+  },
+  {
+    text: 'The White Lodge',
+    value: 'whiteLodge',
+  },
+  {
+    text: 'The Black Lodge',
+    value: 'blackLodge',
+  },
+];
+
+export const timeOptions = [
+  { text: '10 am', value: '1000' },
+  { text: '11 am', value: '1100' },
+  { text: '12 noon', value: '1200' },
+  { text: '1 pm', value: '1300' },
+  { text: '2 pm', value: '1400' },
+  { text: '3 pm', value: '1500' },
+];
+
+export const timeZoneOptions = [
+  { text: 'CET', value: 'cet' },
+  { text: 'GMT', value: 'gmt' },
+  { text: 'EST', value: 'est' },
+];
