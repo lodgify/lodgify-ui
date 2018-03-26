b5ce442255f9c861f6d47d7aedf515e07b256db5
Marc <marc@lodgify.com>
2018-03-26 16:22:01 +0200

    Align images props with Image widget

--- src/components/elements/Slideshow/Readme.md
+++
@@ -1,5 +0,0 @@
-```jsx
-const { imageUrls } = require('./mock-data/imageUrls');
-
-<Slideshow imageUrls={imageUrls} />
-```
---
+++ src/components/widgets/Slideshow/component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery-no-icon.css';
+
+import { adaptImages } from './utils/adaptImages';
+import { renderNavButton } from './utils/renderNavButton';
+
+/**
+ * A slideshow displays a series of images.
+ * @return {Object}
+ */
+export const Component = ({ images }) => (
+  <ImageGallery
+    items={adaptImages(images)}
+    lazyLoad
+    // Note: styles for the pagination controls
+    // live in `semantic/src/themes/livingstone/collections/menu.*`
+    renderRightNav={renderNavButton('right')}
+    renderLeftNav={renderNavButton('left')}
+    showBullets
+    showFullscreenButton={false}
+    showPlayButton={false}
+    showThumbnails={false}
+  />
+);
+
+Component.displayName = 'Slideshow';
+
+Component.propTypes = {
+  /**  The images to display.  */
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      /** Alternative text to show if the image can't be loaded by the browser. */
+      alternativeText: PropTypes.string,
+      /** URL pointing to the image to display. */
+      url: PropTypes.string.isRequired,
+      /** A set of media conditions indicating to the browser which source to choose.
+       *  [See this for more info](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images)
+       */
+      sizes: PropTypes.string.isRequired,
+      /** The set of images the browser can choose between depending on screen width.
+       *  [See this for more info](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images)
+       */
+      sourceSet: PropTypes.string.isRequired,
+      /** Title of the image to show when hovering over it on desktop browsers. */
+      title: PropTypes.string,
+    })
+  ).isRequired,
+};
