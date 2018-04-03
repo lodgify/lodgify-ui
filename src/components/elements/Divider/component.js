c7e37b5dd3a8d6c9d9fbdfc9aead77af0187a53b
Marc <marc@lodgify.com>
2018-04-03 13:20:19 +0200

    Add hasLabel prop to Divider component. Update tests

--- src/components/elements/Divider/component.js
+++ src/components/elements/Divider/component.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Divider } from 'semantic-ui-react';
 
 /**
  * A divider adds whitespace between other elements.
  * @return {Object}
  */
-export const Component = () => <Divider hidden />;
+export const Component = ({ hasLine }) => <Divider hidden={!hasLine} />;
 
 Component.displayName = 'Divider';
+
+Component.defaultProps = {
+  hasLine: false,
+};
+
+Component.propTypes = {
+  /** Does the divider have a visible line. */
+  hasLine: PropTypes.bool,
+};
