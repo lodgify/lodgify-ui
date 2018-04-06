2c76ec40c7f07193d588ae81139e8223067c0050
Marc <marc@lodgify.com>
2018-04-06 16:08:52 +0200

    Add examples

---
+++ src/components/widgets/Amenities/mock-data/amenities.js
@@ -0,0 +1,23 @@
+export const sixAmenities = [
+  { iconName: 'fire', label: 'Fireplace' },
+  { iconName: 'print', label: 'Home office' },
+  { iconName: 'child', label: 'Child friendly' },
+  { iconName: 'paw', isDisabled: true, label: 'No pets' },
+  { iconName: 'leaf', label: 'Clean energy' },
+  { iconName: 'cocktail', label: 'Parties' },
+];
+
+export const twelveAmenities = [
+  { iconName: 'fire', label: 'Fireplace' },
+  { iconName: 'print', label: 'Home office' },
+  { iconName: 'child', label: 'Child friendly' },
+  { iconName: 'paw', isDisabled: true, label: 'No pets' },
+  { iconName: 'leaf', label: 'Clean energy' },
+  { iconName: 'cocktail', label: 'Parties' },
+  { iconName: 'signal', label: 'Phone signal' },
+  { iconName: 'shop', label: 'Shop nearby' },
+  { iconName: 'bug', isDisabled: true, label: 'Bug free' },
+  { iconName: 'heartbeat', label: 'Defibrillator' },
+  { iconName: 'birthday', label: 'Pastry chef' },
+  { iconName: 'legal', label: 'Legal' },
+];
