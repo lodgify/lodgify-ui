495c1c48bba98718407d978e5b296b1d88205c4f
Marc <marc@lodgify.com>
2018-04-11 16:15:53 +0200

    Create proposed expect helpers to check props and children. Isolate in lib/

--- src/components/elements/Button/component.spec.js
+++ src/components/elements/Button/component.spec.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Button as SemanticButton } from 'semantic-ui-react';
+
+import {
+  expectComponentToHaveChildren,
+  expectComponentToHaveProps,
+} from 'lib/expect-helpers';
 
 import { Component as Button } from './component';
 
@@ -8,83 +13,103 @@ const getButton = props => shallow(<Button {...props}>Press me</Button>);
 
 describe('<Button />', () => {
   it('should render a single Semantic UI `Button` component', () => {
-    const button = getButton();
-    const actual = button.find('Button').length;
-    expect(actual).toBe(1);
+    const wrapper = getButton();
+    const actual = wrapper.is(SemanticButton);
+    expect(actual).toBe(true);
   });
 
   it('should pass the `Button` component the right props', () => {
-    const button = getButton();
-    const actual = button.find('Button').props();
-    expect(actual).toEqual(
-      expect.objectContaining({
-        disabled: false,
-        floated: 'left',
-        loading: false,
-        circular: false,
-      })
-    );
+    const wrapper = getButton();
+    expectComponentToHaveProps(wrapper, {
+      disabled: false,
+      floated: 'left',
+      loading: false,
+      circular: false,
+    });
   });
 
   it('should pass the `Button` component `props.isRounded` as `circular`', () => {
-    const button = getButton({ isRounded: true });
-    const actual = button.find('Button').props();
-    expect(actual).toEqual(
-      expect.objectContaining({
-        circular: true,
-      })
-    );
+    const wrapper = getButton({ isRounded: true });
+    expectComponentToHaveProps(wrapper, {
+      circular: true,
+    });
   });
 
   describe('if `props.isPositionedRight` is true', () => {
     it('should pass the `Button` component `floated="right"`', () => {
-      const button = getButton({ isPositionedRight: true });
-      const actual = button.find('Button').prop('floated');
-      expect(actual).toBe('right');
+      const wrapper = getButton({ isPositionedRight: true });
+      expectComponentToHaveProps(wrapper, {
+        floated: 'right',
+      });
     });
   });
 
   describe('if `props.isSecondary` is true', () => {
     it('should pass the `Button` component `secondary={true}`', () => {
-      const button = getButton({ isSecondary: true });
-      const actual = button.find('Button').prop('secondary');
-      expect(actual).toBe(true);
+      const wrapper = getButton({ isSecondary: true });
+      expectComponentToHaveProps(wrapper, {
+        secondary: true,
+      });
     });
   });
 
   describe('if `props.size` is informed', () => {
     it('should pass the `Button` component `size={size}`', () => {
-      const button = getButton({ size: 'massive' });
-      const actual = button.find('Button').prop('size');
-      expect(actual).toBe('massive');
+      const wrapper = getButton({ size: 'massive' });
+      expectComponentToHaveProps(wrapper, {
+        size: 'massive',
+      });
     });
   });
 
+  it('should render the right children', () => {
+    const wrapper = getButton();
+    expectComponentToHaveChildren(wrapper, 'Press me');
+  });
+
   describe('if `props.icon` is informed', () => {
-    it('should render an <Icon /> if is string', () => {
-      const button = getButton({ icon: 'world' });
-      const actual = button.find(Icon).length;
-      expect(actual).toBe(1);
+    const getButtonWithIcon = () => getButton({ icon: 'world' });
+
+    it('should render a <span>', () => {
+      const wrapper = getButtonWithIcon();
+      expectComponentToHaveChildren(wrapper, 'span', 'Press me');
     });
 
-    it('should render with `has-icon` class name', () => {
-      const button = getButton({ icon: 'world' });
-      const actual = button.find('.has-icon');
-      expect(actual).toHaveLength(1);
+    it('should have the `has-icon` className', () => {
+      const wrapper = getButtonWithIcon();
+      expectComponentToHaveProps(wrapper, {
+        className: 'has-icon',
+      });
+    });
+
+    describe('the <span>', () => {
+      it('should render an <Icon />', () => {
+        const wrapper = getButtonWithIcon().children('span');
+        expectComponentToHaveChildren(wrapper, Icon);
+      });
+    });
+
+    describe('the <Icon>', () => {
+      it('should have the right props', () => {
+        const wrapper = getButtonWithIcon().find(Icon);
+        expectComponentToHaveProps(wrapper, {
+          name: 'world',
+        });
+      });
     });
   });
 
   describe('if `props.hasShadow` is informed', () => {
-    it('should render with `has-shadow` class name', () => {
-      const button = getButton({ hasShadow: true });
-      const actual = button.find('.has-shadow');
-      expect(actual).toHaveLength(1);
+    it('should have the `has-shadow` className', () => {
+      const wrapper = getButton({ hasShadow: true });
+      expectComponentToHaveProps(wrapper, {
+        className: 'has-shadow',
+      });
     });
   });
 
-  it('should render the `children`', () => {
-    const button = getButton();
-    const actual = button.contains('Press me');
-    expect(actual).toBe(true);
+  it('should have displayName `Button`', () => {
+    const actual = Button.displayName;
+    expect(actual).toBe('Button');
   });
 });
