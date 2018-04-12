f2f8372b7f6e34665b3da87a3fb1c24e741df644
Marc <marc@lodgify.com>
2018-04-12 13:11:51 +0200

    Refactor GridColumn out of KeyFacts

--- src/components/widgets/KeyFacts/component.js
+++ src/components/widgets/KeyFacts/component.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Label } from 'semantic-ui-react';
 
 import { getUniqueKey } from 'lib/get-unique-key';
-import { GridColumn } from 'layout/GridColumn';
 import { Heading } from 'typography/Heading';
 import { IconCard } from 'elements/IconCard';
 
@@ -11,8 +10,8 @@ import { IconCard } from 'elements/IconCard';
  * The standard widget for displaying key facts about a property.
  * @returns {Object}
  */
-export const Component = ({ keyFacts, width }) => (
-  <GridColumn width={width}>
+export const Component = ({ keyFacts }) => (
+  <div>
     <Heading size="tiny">Key facts</Heading>
     <Label.Group>
       {keyFacts.map(({ iconName, isDisabled, label }, index) => (
@@ -25,14 +24,10 @@ export const Component = ({ keyFacts, width }) => (
         />
       ))}
     </Label.Group>
-  </GridColumn>
+  </div>
 );
 
 Component.displayName = 'KeyFacts';
-
-Component.defaultProps = {
-  width: 12,
-};
 
 Component.propTypes = {
   /** The key facts to display as icon cards. */
@@ -49,6 +44,4 @@ Component.propTypes = {
       label: PropTypes.string.isRequired,
     })
   ).isRequired,
-  /** The number of columns the widget occupies. */
-  width: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
 };
