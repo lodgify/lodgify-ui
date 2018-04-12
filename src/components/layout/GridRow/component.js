28c3c6c6682f099797be4817699a8be05b0e82da
Jonny Gwillim <jonnygwillim@gmail.com>
2018-04-12 10:42:25 +0200

    Add horizontalAlignContent prop to GridRow

--- src/components/layout/GridRow/component.js
+++ src/components/layout/GridRow/component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
 
 /**
@@ -7,6 +8,22 @@ import { Grid } from 'semantic-ui-react';
  *
  * @returns {Object}
  */
-export const Component = props => <Grid.Row {...props} />;
+export const Component = ({ horizontalAlignContent, ...props }) => (
+  <Grid.Row {...props} textAlign={horizontalAlignContent} />
+);
 
 Component.displayName = 'GridRow';
+
+Component.defaultProps = {
+  horizontalAlignContent: 'left',
+};
+
+Component.propTypes = {
+  /** Vertically align the content of the column to the bottom, middle or top. */
+  horizontalAlignContent: PropTypes.oneOf([
+    'left',
+    'center',
+    'right',
+    'justified',
+  ]),
+};
