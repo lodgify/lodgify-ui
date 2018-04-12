4cf27b18174e1a350b501942caa8e8d34c16a08d
Marc <marc@lodgify.com>
2018-04-12 14:02:59 +0200

    Refactor Grid* out of Container

--- src/components/widgets/Container/Readme.md
+++ src/components/widgets/Container/Readme.md
@@ -3,13 +3,3 @@
    <OwnerLogin />
  </Container>
 ```
-
-### Variations
-
-#### Width
-
-```jsx
- <Container width={6}>
-   <OwnerLogin />
- </Container>
-```
--- src/components/widgets/Container/component.js
+++ src/components/widgets/Container/component.js
@@ -1,38 +1,19 @@
-import React, { Children } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Grid } from 'layout/Grid';
-import { GridRow } from 'layout/GridRow';
-import { GridColumn } from 'layout/GridColumn';
-
 /**
- * A container which renders widgets inside of it.
- * By default spans to 100% width stacking widgets vertically.
- *
+ * A container which renders other widgets inside of it.
  * @returns {Object}
  */
-export const Component = ({ children, width }) => (
-  <Grid>
-    <GridRow>
-      <GridColumn width={width}>
-        {Children.map(children, (child, index) =>
-          React.cloneElement(child, { key: index })
-        )}
-      </GridColumn>
-    </GridRow>
-  </Grid>
-);
+export const Component = ({ children }) => <div>{children}</div>;
 
 Component.displayName = 'Container';
 
 Component.defaultProps = {
   children: null,
-  width: 12,
 };
 
 Component.propTypes = {
   /** The children widgets. */
   children: PropTypes.node,
-  /** Value between 1 and 12 */
-  width: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
 };
