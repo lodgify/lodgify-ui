2e0d6103bb0c4a49f41556459b0fe5335fe16871
Jonny Gwillim <jonnygwillim@gmail.com>
2018-04-12 17:09:51 +0200

    Add mock data

---
+++ src/components/widgets/Review/mock-data/props.js
@@ -0,0 +1,17 @@
+export const ratingNumber = 4;
+
+export const reviewerCategory = 'Young couple';
+
+export const reviewerLocation = 'Portugal';
+
+export const reviewerName = 'Magellan';
+// TODO just date and make helper
+export const reviewerStayDate = 'stayed in 9/2015';
+
+export const reviewResponse =
+  'You can also personally respond to each review: Thanks for you kind review, James! Hope to welcome you back soon! Loren ipsum :) ';
+
+export const reviewText =
+  'Beautifully located and well-kept villas in Santorini. Would certainly come back next year.';
+
+export const reviewTitle = 'Great accommodation! Honorable host.';
