8b69b364faae819ed5220352e0dc2076c1f0e920
Marc <marc@lodgify.com>
2018-04-12 18:55:23 +0200

    Make consequent changes to test files

--- src/components/elements/GoogleMap/component.spec.js
+++ src/components/elements/GoogleMap/component.spec.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Card } from 'semantic-ui-react';
 
 import { ReactGoogleMap } from 'lib/react-google-maps';
+import { expectComponentToHaveProps } from 'lib/expect-helpers';
 
 import { Component as GoogleMap } from './component';
 
@@ -15,21 +17,21 @@ const getGoogleMap = () => shallow(<GoogleMap {...props} />);
 describe('<GoogleMap />', () => {
   it('should render a single `ReactGoogleMap` component', () => {
     const wrapper = getGoogleMap();
-    const actual = wrapper.find(ReactGoogleMap);
-    expect(actual).toHaveLength(1);
+    const actual = wrapper.is(ReactGoogleMap);
+    expect(actual).toBe(true);
   });
 
   describe('the `ReactGoogleMap` component', () => {
     it('should get the right props', () => {
       const wrapper = getGoogleMap();
-      const actual = wrapper.find(ReactGoogleMap).props();
-      expect(actual).toEqual(
-        expect.objectContaining({
-          isShowingExactLocation: false,
-          isShowingApproximateLocation: false,
-          ...props,
-        })
-      );
+      expectComponentToHaveProps(wrapper, {
+        apiKey: 'AIzaSyADAwsct9hJOd8t4ljkqGvo59nvTkBZh3Q',
+        containerElement: <Card fluid />,
+        height: '400px',
+        isShowingExactLocation: false,
+        isShowingApproximateLocation: false,
+        ...props,
+      });
     });
   });
 
