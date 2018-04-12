4e3aed0009ecc861ddc114a3d603cf8fb3b72184
Jonny Gwillim <jonnygwillim@gmail.com>
2018-04-12 15:45:32 +0200

    Tidy up getQuoteSource

--- src/components/elements/Quote/utils/getQuoteSource.js
+++ src/components/elements/Quote/utils/getQuoteSource.js
@@ -1,8 +1,7 @@
 /**
- * Creates quote source from quote source and the quote's date and time
- * @param  {string} quoteSource   Name of the person being quoted
- * @param  {string} quoteDateTime Date and time
- * @return {string}               Name, Date and Time concatinated
+ * @param  {String} quoteSource
+ * @param  {String} quoteDateTime
+ * @return {String}
  */
 
 export const getQuoteSource = (quoteSource, quoteDateTime) =>
