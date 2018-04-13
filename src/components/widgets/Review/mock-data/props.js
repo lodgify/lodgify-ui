5e8e3ab6f7bd09157188635c86721ae2f9b347d2
Jonny Gwillim <jonnygwillim@gmail.com>
2018-04-13 11:32:24 +0200

    Update mock-data props

--- src/components/widgets/Review/mock-data/props.js
+++ src/components/widgets/Review/mock-data/props.js
@@ -6,14 +6,14 @@ export const reviewerLocation = 'Portugal';
 
 export const reviewerName = 'Magellan';
 
-export const reviewerStayDate = '9/2015';
-
 export const reviewResponse = {
+  dateTime: '10/14/2015 12:22:58 PM',
   text:
     'You can also personally respond to each review: Thanks for you kind review, James! Hope to welcome you back soon!',
   source: 'The Owner',
-  dateTime: '10/14/2015 12:22:58 PM',
 };
+
+export const reviewerStayDate = '9/2015';
 
 export const reviewText =
   'Beautifully located and well-kept villas in Santorini. Would certainly come back next year.';
