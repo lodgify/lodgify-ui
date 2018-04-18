4ef92a139af58f9f5c57525e3cae0f072147be70
Jonny Gwillim <jonnygwillim@gmail.com>
2018-04-18 15:40:55 +0200

    add semantic stackable prop

--- src/components/widgets/PropertyDescription/component.js
+++ src/components/widgets/PropertyDescription/component.js
@@ -21,7 +21,7 @@ export const Component = ({
   icons,
   propertyType,
 }) => (
-  <Grid>
+  <Grid stackable columns={2}>
     <GridColumn width={7}>
       <Paragraph size="tiny">{propertyType}</Paragraph>
       {getParagraphsFromStrings(descriptionText).map((paragraphText, index) => (
