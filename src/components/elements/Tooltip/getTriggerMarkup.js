4a7d043e780520f18881f822600bff3efdd1e4b5
Marc <marc@lodgify.com>
2018-04-19 16:02:05 +0200

    Isolate markup for Tooltip trigger

---
+++ src/components/elements/Tooltip/getTriggerMarkup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Icon } from 'elements/Icon';
+
+export const getTriggerMarkup = size => (
+  <Icon name="info" color="grey" isColorInverted isCircular size={size} />
+);
