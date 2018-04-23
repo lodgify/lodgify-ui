293a498b11967a13167648fefb475fd0b6815dc4
David Pelayo <hi@davidpelayo.com>
2018-04-23 11:42:22 +0200

    TE-336 Grid, GridColumn and GridRow tests

---
+++ src/components/layout/Grid/component.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Grid } from 'semantic-ui-react';
+
+import {
+  expectComponentToHaveProps,
+  expectComponentToHaveDisplayName,
+  expectComponentToBe,
+} from 'lib/expect-helpers';
+
+import { Component as LodgifyGrid } from './component';
+
+const getGrid = props => shallow(<LodgifyGrid {...props} />);
+
+describe('<Grid />', () => {
+  it('should render a single Semantic UI `Grid` component', () => {
+    const wrapper = getGrid();
+    expectComponentToBe(wrapper, Grid);
+  });
+
+  describe('the `Grid` component', () => {
+    it('should get the right props', () => {
+      const wrapper = getGrid();
+      expectComponentToHaveProps(wrapper, {
+        centered: expect.any(Boolean),
+      });
+    });
+  });
+
+  it('should have displayName `Grid`', () => {
+    expectComponentToHaveDisplayName(LodgifyGrid, 'Grid');
+  });
+});
