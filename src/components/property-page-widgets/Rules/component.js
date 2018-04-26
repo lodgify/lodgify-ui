3f60b3569be269c64ad0fbf17c4232780c42bc41
Marc <marc@lodgify.com>
2018-04-26 14:37:45 +0200

    Simplify Rules widget and fix responsive behaviour

--- src/components/property-page-widgets/Rules/component.js
+++ src/components/property-page-widgets/Rules/component.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'semantic-ui-react';
 
-import { getParagraphsFromStrings } from 'lib/get-paragraphs-from-strings';
 import { Grid } from 'layout/Grid';
 import { GridColumn } from 'layout/GridColumn';
+import { Divider } from 'elements/Divider';
 import { Heading } from 'typography/Heading';
+import { Paragraph } from 'typography/Paragraph';
 import { Icon } from 'elements/Icon';
 
 /**
@@ -15,36 +16,15 @@ import { Icon } from 'elements/Icon';
 export const Component = ({ checkInTime, checkOutTime, rules }) => (
   <Grid stackable>
     <GridColumn width={12}>
-      <Heading size="small">House Rules</Heading>
+      <Heading>House Rules</Heading>
     </GridColumn>
-    <GridColumn width={3}>
-      <List items={getParagraphsFromStrings(rules.join('\n'))} />
+    <GridColumn computer={3} tablet={5}>
+      <List items={rules.map(rule => <Paragraph>{rule}</Paragraph>)} />
     </GridColumn>
-    <GridColumn width={2}>
-      <Grid verticalAlign="middle">
-        <GridColumn width={3}>
-          <Icon name="bed" />
-        </GridColumn>
-        <GridColumn width={9}>
-          <div>
-            <b>Check-in</b>
-          </div>
-          <span className="header">{checkInTime}</span>
-        </GridColumn>
-      </Grid>
-    </GridColumn>
-    <GridColumn width={2}>
-      <Grid verticalAlign="middle">
-        <GridColumn width={3}>
-          <Icon name="question mark" />
-        </GridColumn>
-        <GridColumn width={9}>
-          <div>
-            <b>Check-out</b>
-          </div>
-          <span className="header">{checkOutTime}</span>
-        </GridColumn>
-      </Grid>
+    <GridColumn computer={9} tablet={7}>
+      <Icon label={`Check in: ${checkInTime}`} name="checkin" />
+      <Divider />
+      <Icon label={`Check out: ${checkOutTime}`} name="checkout" />
     </GridColumn>
   </Grid>
 );
