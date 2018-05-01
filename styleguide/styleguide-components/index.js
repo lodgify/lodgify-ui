735803d6575334c6a023a9efc8315badbffe658b
Marc <marc@lodgify.com>
2018-05-01 16:39:25 +0200

    Add custom PlaygroundRenderer component with conditional playground rendering.

--- styleguide/styleguide-components/index.js
+++ styleguide/styleguide-components/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   ComponentsListRenderer: path.join(__dirname, 'ComponentsListRenderer'),
   LogoRenderer: path.join(__dirname, 'LogoRenderer'),
+  PlaygroundRenderer: path.join(__dirname, 'PlaygroundRenderer'),
   SectionHeadingRenderer: path.join(__dirname, 'SectionHeadingRenderer'),
   StyleGuideRenderer: path.join(__dirname, 'StyleGuideRenderer'),
 };
