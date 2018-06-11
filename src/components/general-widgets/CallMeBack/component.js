13ec23e41cb93e486265c281da0088549700d5fb
Jonny Gwillim <jonnygwillim@gmail.com>
2018-06-11 13:03:22 +0200

    fix(CallMeBack): replace date picker Dropdown with SingleDatePicker

--- src/components/general-widgets/CallMeBack/component.js
+++ src/components/general-widgets/CallMeBack/component.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Dropdown } from 'inputs/Dropdown';
 import { Form } from 'collections/Form';
 import { InputGroup } from 'collections/InputGroup';
-import { TextInput } from 'inputs/TextInput';
 import { PhoneInput } from 'inputs/PhoneInput';
-import { Dropdown } from 'inputs/Dropdown';
+import { SingleDatePicker } from 'inputs/SingleDatePicker';
 import { TextArea } from 'inputs/TextArea';
+import { TextInput } from 'inputs/TextInput';
 
 /**
  * The standard widget for a user to request a call back.
  * @returns {Object}
  */
 export const Component = ({
-  dateOptions,
   onSubmit,
   propertyOptions,
   timeOptions,
@@ -26,12 +26,7 @@ export const Component = ({
     </InputGroup>
     <TextInput label="Email" name="email" />
     <InputGroup>
-      <Dropdown
-        icon="calendar"
-        label="Date"
-        name="date"
-        options={dateOptions}
-      />
+      <SingleDatePicker name="date" placeholderText="Date" />
       <Dropdown icon="clock" label="Time" name="time" options={timeOptions} />
     </InputGroup>
     <InputGroup>
@@ -49,15 +44,6 @@ Component.defaultProps = {
 };
 
 Component.propTypes = {
-  /** The options which the user can select for the date field. */
-  dateOptions: PropTypes.arrayOf(
-    PropTypes.shape({
-      /** The visible text for the option. */
-      text: PropTypes.string.isRequired,
-      /** The underlying value for the option. */
-      value: PropTypes.any,
-    })
-  ).isRequired,
   /** The function to call when the form is submitted
    *  @param {Object} values - The values of the inputs in the form.
    */
