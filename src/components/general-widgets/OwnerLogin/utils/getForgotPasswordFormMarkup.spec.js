976b814b7644751209ff1ff904130fe8c92879f3
Marc <marc@lodgify.com>
2018-06-12 16:24:45 +0200

    test(OwnerLogin): add tests for forgot password form modal

---
+++ src/components/general-widgets/OwnerLogin/utils/getForgotPasswordFormMarkup.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Form } from 'collections/Form';
+import { Modal } from 'elements/Modal';
+import { TextInput } from 'inputs/TextInput';
+
+import { getForgotPasswordFormMarkup } from './getForgotPasswordFormMarkup';
+
+describe('getForgotPasswordFormMarkup', () => {
+  it('should return the right markup ', () => {
+    const someFunction = () => {};
+    const actual = getForgotPasswordFormMarkup(someFunction);
+    expect(actual).toEqual(
+      <Modal trigger={<span>Forgot password</span>}>
+        <Form
+          headingText="Forgot Password"
+          onSubmit={someFunction}
+          submitButtonText="Send reset"
+        >
+          <TextInput label="Email" name="email" />
+        </Form>
+      </Modal>
+    );
+  });
+});
