dafb4409cde5b9c529073d60acf46c472f721ed3
Marc <marc@lodgify.com>
2018-06-14 11:50:42 +0200

    feat(FeaturedRoomTypes): add component with examples and expose to the api

---
+++ src/components/general-widgets/FeaturedRoomTypes/component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Grid } from 'layout/Grid';
+import { GridColumn } from 'layout/GridColumn';
+import { Heading } from 'typography/Heading';
+import { FeaturedRoomType } from 'general-widgets/FeaturedRoomType';
+import { buildKeyFromStrings } from 'utils/build-key-from-strings';
+
+/**
+ * The standard widget for displaying a list of featured rooms.
+ * @returns {Object}
+ */
+export const Component = ({ featuredRoomTypes, headingText }) => (
+  <Grid>
+    {headingText && (
+      <GridColumn width={12}>
+        <Heading>{headingText}</Heading>
+      </GridColumn>
+    )}
+    {featuredRoomTypes.map((featuredRoomType, index) => (
+      <GridColumn
+        computer={4}
+        key={buildKeyFromStrings(featuredRoomType.roomTypeName, index)}
+        mobile={12}
+        tablet={6}
+      >
+        <FeaturedRoomType {...featuredRoomType} />
+      </GridColumn>
+    ))}
+  </Grid>
+);
+
+Component.displayName = 'FeaturedRoomTypes';
+
+Component.defaultProps = {
+  headingText: null,
+};
+
+Component.propTypes = {
+  /** An array of [`FeaturedRoomType`](/#!/FeaturedRoomType) props objects. */
+  featuredRoomTypes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  /** The text to display as a heading at the top of the widget. */
+  headingText: PropTypes.string,
+};
