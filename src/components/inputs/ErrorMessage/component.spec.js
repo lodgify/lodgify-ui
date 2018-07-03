8b73fdd977a86f230485236da535e827e02762f2
Jonny Gwillim <jonnygwillim@gmail.com>
2018-07-03 17:22:35 +0200

    refactor(ErrorMessage): update spec to use @lodgify expect helpers

--- src/components/inputs/ErrorMessage/component.spec.js
+++ src/components/inputs/ErrorMessage/component.spec.js
@@ -3,31 +3,36 @@ import { shallow } from 'enzyme';
 import { Label } from 'semantic-ui-react';
 import {
   expectComponentToBe,
+  expectComponentToHaveChildren,
   expectComponentToHaveDisplayName,
+  expectComponentToHaveProps,
 } from '@lodgify/enzyme-jest-expect-helpers';
 
 import { Component as ErrorMessage } from './component';
 
 const errorMessage = '🔥';
 
+const getErrorMessage = () =>
+  shallow(<ErrorMessage errorMessage={errorMessage} />);
+
 describe('<ErrorMessage />', () => {
   it('should render a single Semantic UI `Label` component', () => {
-    const wrapper = shallow(<ErrorMessage errorMessage={errorMessage} />);
+    const wrapper = getErrorMessage();
     expectComponentToBe(wrapper, Label);
   });
 
-  it('should pass the right props to `Label`', () => {
-    const textInput = shallow(<ErrorMessage errorMessage={errorMessage} />);
-    const actual = textInput.find('Label').props();
-    expect(actual).toEqual(
-      expect.objectContaining({ color: 'red', pointing: 'below' })
-    );
-  });
+  describe('`Label`', () => {
+    const getLabel = () => getErrorMessage().find('Label');
 
-  it('should render `props.errorMessage`', () => {
-    const textInput = shallow(<ErrorMessage errorMessage={errorMessage} />);
-    const actual = textInput.contains(errorMessage);
-    expect(actual).toBe(true);
+    it('should have the right props', () => {
+      const wrapper = getLabel();
+      expectComponentToHaveProps(wrapper, { color: 'red', pointing: 'below' });
+    });
+
+    it('should render the right children', () => {
+      const wrapper = getErrorMessage();
+      expectComponentToHaveChildren(wrapper, errorMessage);
+    });
   });
 
   it('should have displayName `ErrorMessage`', () => {
