aef88974be678c6c167f934660a4ed6088e114d8
Jonny Gwillim <jonnygwillim@gmail.com>
2018-07-09 13:18:52 +0200

    feat(Slideshow): expose additional formatting props

--- src/components/media/Slideshow/component.js
+++ src/components/media/Slideshow/component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ImageGallery from 'react-image-gallery';
+import getClassNames from 'classnames';
 
 import { adaptImages } from './utils/adaptImages';
 import { renderNavButton } from './utils/renderNavButton';
@@ -9,25 +10,44 @@ import { renderNavButton } from './utils/renderNavButton';
  * A slideshow displays a series of images.
  * @return {Object}
  */
-export const Component = ({ images, ...props }) => (
+export const Component = ({
+  hasShadow,
+  images,
+  isRounded,
+  isShowingBulletNavigation,
+  isStretched,
+}) => (
   <ImageGallery
-    items={adaptImages(images)}
-    lazyLoad
+    additionalClass={getClassNames('', {
+      'fit-height': isStretched,
+      'no-shadow': !hasShadow,
+      'no-border-radius': !isRounded,
+    })}
     // Note: styles for the pagination controls
     // live in `styles/semantic/themes/livingstone/collections/menu.*`
+    items={adaptImages(images)}
+    lazyLoad
     renderLeftNav={renderNavButton('left')}
     renderRightNav={renderNavButton('right')}
-    showBullets
+    showBullets={isShowingBulletNavigation}
     showFullscreenButton={false}
     showPlayButton={false}
     showThumbnails={false}
-    {...props}
   />
 );
 
 Component.displayName = 'Slideshow';
 
+Component.defaultProps = {
+  hasShadow: true,
+  isRounded: true,
+  isShowingBulletNavigation: true,
+  isStretched: false,
+};
+
 Component.propTypes = {
+  /** Has the slideshow got a drop shadow  */
+  hasShadow: PropTypes.bool,
   /**  The images to display.  */
   images: PropTypes.arrayOf(
     PropTypes.shape({
@@ -47,4 +67,10 @@ Component.propTypes = {
       url: PropTypes.string.isRequired,
     })
   ).isRequired,
+  /** Is the slideshow formatted to have rounded corners */
+  isRounded: PropTypes.bool,
+  /** Is the slideshow displaying the bullet navigation */
+  isShowingBulletNavigation: PropTypes.bool,
+  /** Is the slideshow fitting the height of it's container */
+  isStretched: PropTypes.bool,
 };
