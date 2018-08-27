a770a62591c18487cb06394571c5f976706c10b7
Stephen Kerr <stephen@lodgify.com>
2018-08-27 14:33:22 +0200

    refactor(HostProfile): improve spec coverage

--- src/components/property-page-widgets/HostProfile/component.spec.js
+++ src/components/property-page-widgets/HostProfile/component.spec.js
@@ -8,6 +8,13 @@ import {
   expectComponentToBe,
 } from '@lodgify/enzyme-jest-expect-helpers';
 
+import {
+  CONTACT_INFORMATION,
+  EMAIL,
+  LANGUAGES,
+  PHONE,
+  YOUR_HOST,
+} from 'utils/default-strings';
 import { Heading } from 'typography/Heading';
 import { Grid } from 'layout/Grid';
 import { GridRow } from 'layout/GridRow';
@@ -19,7 +26,7 @@ import { Component as HostProfile } from './component';
 const catImageUrl =
   'https://si4.cdbcdn.com/oh/4efbc79e-34db-4447-b31a-24e77f33f4e9.jpg?w=1024&mode=max';
 const name = 'Mitjons & Kira';
-const description = `We do love cats`;
+const description = 'We do love cats';
 
 const email = 'welovecats@lodgify.com';
 const phone = '+34932206524';
@@ -57,7 +64,7 @@ describe('<HostProfile />', () => {
       expectComponentToHaveChildren(getGridRowAt(0), GridColumn);
     });
 
-    describe('Its `GridColumn`', () => {
+    describe('The first `GridColumn`', () => {
       const gridColumn = getGridRowAt(0).find(GridColumn);
 
       it('should render a `GridColumn` with the right props', () => {
@@ -70,14 +77,22 @@ describe('<HostProfile />', () => {
         expectComponentToHaveChildren(gridColumn, Heading);
       });
     });
+
+    describe('The first `Heading`', () => {
+      it('should render the right children', () => {
+        const heading = getGridRowAt(0).find(Heading);
+
+        expectComponentToHaveChildren(heading, YOUR_HOST);
+      });
+    });
   });
 
   describe('The second `GridRow`', () => {
-    it('should render a `GridColumn`', () => {
+    it('should render the right children', () => {
       expectComponentToHaveChildren(getGridRowAt(1), GridColumn);
     });
 
-    describe('Its `GridColumn`', () => {
+    describe('the first `GridColumn`', () => {
       const gridColumn = getGridRowAt(1).find(GridColumn);
 
       it('should render a `GridColumn` with the right props', () => {
@@ -86,7 +101,7 @@ describe('<HostProfile />', () => {
         });
       });
 
-      it('should render a `Heading` component', () => {
+      it('should render the right children', () => {
         expectComponentToHaveChildren(gridColumn, Item.Group);
       });
     });
@@ -110,18 +125,28 @@ describe('<HostProfile />', () => {
       expectComponentToHaveChildren(item, Item.Image, Item.Header);
     });
 
-    describe('The `Item.Image` and `Item.Header`', () => {
+    describe('The `Item.Image`', () => {
       it('should be rendered with the right props', () => {
         expectComponentToHaveProps(itemGroup.find(Item.Image), {
           size: 'tiny',
           avatar: true,
           src: expect.any(String),
         });
+      });
+    });
 
-        expectComponentToHaveProps(itemGroup.find(Item.Header), {
+    describe('The `Item.Header`', () => {
+      const itemHeader = itemGroup.find(Item.Header);
+
+      it('should be rendered with the right props', () => {
+        expectComponentToHaveProps(itemHeader, {
           as: 'h4',
           verticalAlign: 'middle',
         });
+      });
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(itemHeader, name);
       });
     });
   });
@@ -134,11 +159,11 @@ describe('<HostProfile />', () => {
       .find(GridColumn)
       .at(1);
 
-    it('should render a `GridColumn`', () => {
+    it('should render the right children', () => {
       expectComponentToHaveChildren(getGridRowAt(2), GridColumn, GridColumn);
     });
 
-    describe('its first `GridColumn`', () => {
+    describe('the first `GridColumn`', () => {
       it('should render with the right props', () => {
         expectComponentToHaveProps(firstColumn, {
           mobile: 12,
@@ -152,7 +177,7 @@ describe('<HostProfile />', () => {
       });
     });
 
-    describe('its second `GridColumn`', () => {
+    describe('the second `GridColumn`', () => {
       it('should render with the right props', () => {
         expectComponentToHaveProps(secondColumn, {
           mobile: 12,
@@ -163,6 +188,120 @@ describe('<HostProfile />', () => {
 
       it('should render the right children', () => {
         expectComponentToHaveChildren(secondColumn, Heading, List);
+      });
+    });
+
+    describe('the first `Paragraph`', () => {
+      const firstParagraph = getGridRows()
+        .find(Paragraph)
+        .at(0);
+
+      it('should render with the right props', () => {
+        expectComponentToHaveProps(firstParagraph, {
+          size: 'medium',
+        });
+      });
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(firstParagraph, description);
+      });
+    });
+
+    describe('the second `Heading`', () => {
+      const secondHeading = getGridRows()
+        .find(Heading)
+        .at(1);
+
+      it('should render with the right props', () => {
+        expectComponentToHaveProps(secondHeading, {
+          size: 'small',
+        });
+      });
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(secondHeading, CONTACT_INFORMATION);
+      });
+    });
+
+    describe('the first `List`', () => {
+      const firstList = getGridRows()
+        .find(List)
+        .at(0);
+
+      it('should render with the right props', () => {
+        expectComponentToHaveProps(firstList, {
+          relaxed: true,
+          size: 'medium',
+        });
+      });
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(
+          firstList,
+          List.Item,
+          List.Item,
+          List.Item
+        );
+      });
+    });
+
+    describe('the first `List.Item`', () => {
+      const ListItem = getGridRows()
+        .find(List.Item)
+        .at(0);
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(ListItem, 'span', 'span');
+      });
+
+      describe('the two inner `span`', () => {
+        it('should render the right children', () => {
+          const firstSpan = ListItem.find('span').at(0);
+          const secondSpan = ListItem.find('span').at(1);
+
+          expectComponentToHaveChildren(firstSpan, `${EMAIL}: `);
+          expectComponentToHaveChildren(secondSpan, email);
+        });
+      });
+    });
+
+    describe('the second `List.Item`', () => {
+      const ListItem = getGridRows()
+        .find(List.Item)
+        .at(1);
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(ListItem, 'span', 'span');
+      });
+
+      describe('the two inner `span`', () => {
+        it('should render the right children', () => {
+          const firstSpan = ListItem.find('span').at(0);
+          const secondSpan = ListItem.find('span').at(1);
+
+          expectComponentToHaveChildren(firstSpan, `${PHONE}: `);
+          expectComponentToHaveChildren(secondSpan, phone);
+        });
+      });
+    });
+
+    describe('the third `List.Item`', () => {
+      const ListItem = getGridRows()
+        .find(List.Item)
+        .at(2);
+
+      it('should render the right children', () => {
+        expectComponentToHaveChildren(ListItem, 'span', 'span');
+      });
+
+      describe('the two inner `span`', () => {
+        it('should render the right children', () => {
+          const firstSpan = ListItem.find('span').at(0);
+          const secondSpan = ListItem.find('span').at(1);
+
+          expectComponentToHaveChildren(firstSpan, `${LANGUAGES}: `);
+          expectComponentToHaveChildren(secondSpan, languages.join(', '));
+        });
       });
     });
   });
