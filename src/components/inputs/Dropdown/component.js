6c9ad9fc38785a222def5ad5ab27f983acf8246b
Stephen Kerr <stephen@lodgify.com>
2018-08-30 11:31:18 +0200

    feat(Dropdown): add dropdown error validation state

--- src/components/inputs/Dropdown/component.js
+++ src/components/inputs/Dropdown/component.js
@@ -5,9 +5,12 @@ import getClassNames from 'classnames';
 
 import { Icon, ICON_NAMES } from 'elements/Icon';
 
+import { ErrorMessage } from '../ErrorMessage';
+
 import { adaptOptions } from './utils/adaptOptions';
 import { getDefaultValue } from './utils/getDefaultValue';
 import { getHasImages } from './utils/getHasImages';
+import { getHasErrorMessage } from './utils/getHasErrorMessage';
 
 /**
  * A dropdown allows a user to select a value from a series of options.
@@ -42,27 +45,40 @@ export class Component extends PureComponent {
    */
   handleOpen = isOpen => this.setState({ isOpen });
 
+  /**
+   * Handle blur event
+   */
+  handleBlur = () => {
+    this.props.onBlur();
+    this.handleOpen(false);
+  };
+
   render() {
     const { isOpen, value } = this.state;
-    const { icon, isDisabled, label, options } = this.props;
+    const { icon, isDisabled, label, options, error, isValid } = this.props;
     const hasImages = getHasImages(options);
     const adaptedOptions = adaptOptions(options, hasImages);
     const defaultValue = getDefaultValue(adaptedOptions, hasImages, !!label);
+    const hasErrorMessage = getHasErrorMessage(error);
 
     return (
       <div
-        className={getClassNames('dropdown-container', {
-          dirty: value,
+        className={getClassNames('dropdown-container', 'ui', 'input', {
           'has-images': hasImages,
+          dirty: value,
+          error: error,
           focus: isOpen,
+          valid: isValid,
         })}
       >
+        {hasErrorMessage && <ErrorMessage errorMessage={error} />}
+        {isValid && <Icon color="green" name={ICON_NAMES.CHECKMARK} />}
         {!hasImages && icon && <Icon name={icon} />}
         <Dropdown
           defaultValue={defaultValue}
           disabled={isDisabled || !adaptedOptions.length}
           icon={<Icon name={ICON_NAMES.CARET_DOWN} />}
-          onBlur={() => this.handleOpen(false)}
+          onBlur={this.handleBlur}
           onChange={this.handleChange}
           onClick={() => this.handleOpen(!isOpen)}
           open={isOpen}
@@ -79,23 +95,35 @@ export class Component extends PureComponent {
 Component.displayName = 'Dropdown';
 
 Component.defaultProps = {
-  isDisabled: false,
+  error: false,
   icon: null,
+  isDisabled: false,
+  isValid: false,
   label: '',
   name: '',
+  onBlur: Function.prototype,
   onChange: Function.prototype,
   options: [],
 };
 
 Component.propTypes = {
+  /** Is the dropdown in an error state. */
+  error: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
   /** Icon for the dropdown. Not displayed if options have images. */
   icon: PropTypes.string,
   /** A disabled dropdown does not allow user interaction. */
   isDisabled: PropTypes.bool,
+  /** Is the dropdown in a valid state. */
+  isValid: PropTypes.bool,
   /** The label for the dropdown. Not displayed if options have images. */
   label: PropTypes.string,
   /** The name for the dropdown. */
   name: PropTypes.string,
+  /**
+   * Used internally by `Form` so ignored in the styleguide.
+   * @ignore
+   */
+  onBlur: PropTypes.func,
   /** A function called when the dropdown value changes. */
   onChange: PropTypes.func,
   /** The options which the user can select. Dropdown is disabled if options is an empty array. */
