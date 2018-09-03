88a6881c82cd114c1cd1ec28c33b8421cfd49282
Stephen Kerr <stephen@lodgify.com>
2018-09-03 12:27:24 +0200

    refactor(Container): clean up review and lint issues

--- src/components/layout/Container/component.js
+++ src/components/layout/Container/component.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Container as SemanticContainer } from 'semantic-ui-react';
 
 /**
  * Container is the Lodgify UI interface for the
  * Semantic UI Container.
- *
  * @returns {Object}
  */
 export const Component = ({ textAlign, ...props }) => (
@@ -13,12 +11,3 @@ export const Component = ({ textAlign, ...props }) => (
 );
 
 Component.displayName = 'Container';
-
-Component.defaultProps = {
-  textAlign: 'left',
-};
-
-Component.propTypes = {
-  /** The text align for nested children. */
-  textAlign: PropTypes.oneOf(['left', 'center', 'right', 'justified']),
-};
--- src/components/layout/Container/component.spec.js
+++ src/components/layout/Container/component.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Container as SemanticContainer } from 'semantic-ui-react';
 import {
-  expectComponentToHaveProps,
   expectComponentToHaveDisplayName,
   expectComponentToBe,
 } from '@lodgify/enzyme-jest-expect-helpers';
@@ -16,16 +15,6 @@ describe('<Container />', () => {
     const wrapper = getContainer();
 
     expectComponentToBe(wrapper, SemanticContainer);
-  });
-
-  describe('the Semantic UI `Container` component', () => {
-    it('should get the right props', () => {
-      const wrapper = getContainer();
-
-      expectComponentToHaveProps(wrapper, {
-        textAlign: 'left',
-      });
-    });
   });
 
   it('should have displayName `Container`', () => {
