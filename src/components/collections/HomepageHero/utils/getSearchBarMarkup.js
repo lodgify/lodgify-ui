9eeae5afc5bce6adc0768ec8b75f209219b111fc
Jonny Gwillim <jonnygwillim@gmail.com>
2018-09-14 15:06:13 +0200

    feat(HomepageHero): implement and expose component

---
+++ src/components/collections/HomepageHero/utils/getSearchBarMarkup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Grid } from 'layout/Grid';
+import { GridRow } from 'layout/GridRow';
+import { SearchBar } from 'general-widgets/SearchBar';
+
+/**
+ * @param  {Object} props
+ * @return {Object}
+ */
+export const getSearchBarMarkup = props => (
+  <Grid areColumnsCentered>
+    <GridRow horizontalAlignContent="center">
+      <SearchBar {...props} />
+    </GridRow>
+  </Grid>
+);
