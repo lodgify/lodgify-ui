46024aaddfb5c384b542a3b819e351483bcd6d07
Stephen Kerr <stephen@lodgify.com>
2018-09-25 10:10:46 +0200

    refactor(Grid): remove has stacked prop

--- src/components/layout/Grid/component.js
+++ src/components/layout/Grid/component.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
-import getClassNames from 'classnames';
 
 /**
  * Grid is the Lodgify UI interface for the
@@ -11,26 +10,17 @@ import getClassNames from 'classnames';
  * definition of Semantic UI.
  */
 // eslint-disable-next-line jsdoc/require-jsdoc
-export const Component = ({ areColumnsCentered, hasFixedWidth, ...props }) => (
-  <Grid
-    className={getClassNames({
-      'has-fixed-width': hasFixedWidth,
-    })}
-    {...props}
-    centered={areColumnsCentered}
-  />
+export const Component = ({ areColumnsCentered, ...props }) => (
+  <Grid {...props} centered={areColumnsCentered} />
 );
 
 Component.displayName = 'Grid';
 
 Component.defaultProps = {
   areColumnsCentered: false,
-  hasFixedWidth: false,
 };
 
 Component.propTypes = {
   /** Are the columns in the grid centered. */
   areColumnsCentered: PropTypes.bool,
-  /** Should the columns stick to their defined width. */
-  hasFixedWidth: PropTypes.bool,
 };
