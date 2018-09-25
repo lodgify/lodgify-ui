1f2917d36210c26fdb8ebe02ad7cefe01fc80264
Stephen Kerr <stephen@lodgify.com>
2018-09-25 15:51:25 +0200

    refactor(HTML): remove VerticalGutters

--- src/components/general-widgets/HTML/component.js
+++ src/components/general-widgets/HTML/component.js
@@ -2,8 +2,6 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import DOMPurify from 'dompurify';
 
-import { VerticalGutters } from 'layout/VerticalGutters';
-
 /**
  * The HTML widget sanitises and renders HTML strings.
  * @extends {React.PureComponent}
@@ -28,16 +26,12 @@ export class Component extends PureComponent {
     const { children } = this.props;
 
     return children ? (
-      <VerticalGutters>
-        <div>
-          <div dangerouslySetInnerHTML={{ __html: cleanHTMLString }} />
-          {children}
-        </div>
-      </VerticalGutters>
-    ) : (
-      <VerticalGutters>
+      <div>
         <div dangerouslySetInnerHTML={{ __html: cleanHTMLString }} />
-      </VerticalGutters>
+        {children}
+      </div>
+    ) : (
+      <div dangerouslySetInnerHTML={{ __html: cleanHTMLString }} />
     );
   }
 }
