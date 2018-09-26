88a98a5653769e8f399d64285f1559dc06480013
Marc <marc@lodgify.com>
2018-09-26 13:01:02 +0100

    feat(Rating): expose component with tests and examples

---
+++ src/components/elements/Rating/Readme.md
@@ -0,0 +1,23 @@
+```jsx
+<Rating ratingNumber={4.8} />
+```
+
+### Content
+
+### Numeral
+
+```jsx
+<Rating ratingNumber={4.8} />
+<Divider />
+<Rating isShowingNumeral={false} ratingNumber={4.8} />
+```
+
+### Variations
+
+#### Icon size
+
+```jsx
+<Rating ratingNumber={4.8} iconSize="small" />
+<Divider />
+<Rating ratingNumber={4.8} iconSize="tiny" />
+```
---
+++ src/components/elements/Rating/component.spec.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Rating as SemanticRating } from 'semantic-ui-react';
+import {
+  expectComponentToHaveChildren,
+  expectComponentToHaveDisplayName,
+  expectComponentToHaveProps,
+} from '@lodgify/enzyme-jest-expect-helpers';
+
+import { Component as Rating } from './component';
+
+const ratingNumber = 3.4;
+
+const getRating = props =>
+  shallow(<Rating ratingNumber={ratingNumber} {...props} />);
+
+describe('<Rating />', () => {
+  it('should render the right children', () => {
+    const wrapper = getRating();
+
+    expectComponentToHaveChildren(
+      wrapper,
+      ratingNumber.toString(),
+      SemanticRating
+    );
+  });
+
+  describe('if `props.isShowingNumeral` is `false`', () => {
+    it('should render the right children', () => {
+      const wrapper = getRating({ isShowingNumeral: false });
+
+      expectComponentToHaveChildren(wrapper, SemanticRating);
+    });
+  });
+
+  describe('the Semantic UI `Rating`', () => {
+    const getSemanticRating = props => getRating(props).find(SemanticRating);
+
+    it('should get the right props', () => {
+      const wrapper = getSemanticRating();
+
+      expectComponentToHaveProps(wrapper, {
+        disabled: true,
+        maxRating: 5,
+        rating: Math.round(ratingNumber),
+        size: 'small',
+      });
+    });
+
+    describe('if `props.iconSize` is `tiny`', () => {
+      it('should get the right props', () => {
+        const TINY = 'tiny';
+        const wrapper = getSemanticRating({ iconSize: TINY });
+
+        expectComponentToHaveProps(wrapper, {
+          disabled: true,
+          maxRating: 5,
+          rating: Math.round(ratingNumber),
+          size: TINY,
+        });
+      });
+    });
+  });
+
+  it('should have `displayName` Rating', () => {
+    expectComponentToHaveDisplayName(Rating, 'Rating');
+  });
+});
