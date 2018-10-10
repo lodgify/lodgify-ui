12fbf245d98970d4eb5c349757022e846d78b037
Marc <mwgdavies@gmail.com>
2018-10-10 14:32:52 +0800

    fix(SleepingArrangments): consume IconCard with props.size and remove Grid

--- src/components/property-page-widgets/SleepingArrangements/component.spec.js
+++ src/components/property-page-widgets/SleepingArrangements/component.spec.js
@@ -1,81 +1,36 @@
-import React, { Fragment } from 'react';
-import { shallow } from 'enzyme';
-import {
-  expectComponentToBe,
-  expectComponentToHaveChildren,
-  expectComponentToHaveDisplayName,
-  expectComponentToHaveProps,
-} from '@lodgify/enzyme-jest-expect-helpers';
-
-import { SLEEPING_ARRANGEMENTS } from 'utils/default-strings';
-import { Grid } from 'layout/Grid';
-import { GridColumn } from 'layout/GridColumn';
-import { Heading } from 'typography/Heading';
-import { IconCard } from 'elements/IconCard';
+import React from 'react';
+import { mount } from 'enzyme';
 
 import { Component as SleepingArrangements } from './component';
 
 const sleepingArrangements = [
-  { iconName: 'bed', label: '1 king bed' },
-  { iconName: 'bed', label: '2 single beds' },
+  { iconName: 'single bed', label: '1 king bed' },
+  { iconName: 'single bed', label: '2 single beds' },
   { iconName: 'paw', label: '1 kennel' },
 ];
 
-const getSleepingArrangements = () =>
-  shallow(<SleepingArrangements sleepingArrangements={sleepingArrangements} />);
+const getSleepingArrangements = props =>
+  mount(
+    <SleepingArrangements
+      sleepingArrangements={sleepingArrangements}
+      {...props}
+    />
+  );
 
 describe('<SleepingArrangements />', () => {
-  it('should have `Fragment` component as a wrapper', () => {
-    const wrapper = getSleepingArrangements();
+  describe('by default', () => {
+    it('should render the right structure', () => {
+      const actual = getSleepingArrangements();
 
-    expectComponentToBe(wrapper, Fragment);
-  });
-
-  describe('the `Fragment` component', () => {
-    it('should have the right children', () => {
-      const wrapper = getSleepingArrangements();
-
-      expectComponentToHaveChildren(wrapper, Heading, Grid);
+      expect(actual).toMatchSnapshot();
     });
   });
 
-  describe('the `Heading` component', () => {
-    it('should render the right children', () => {
-      const wrapper = getSleepingArrangements().find(Heading);
+  describe('if `props.headingText` is defined', () => {
+    it('should render the right structure', () => {
+      const actual = getSleepingArrangements({ headingText: '👥' });
 
-      expectComponentToHaveChildren(wrapper, SLEEPING_ARRANGEMENTS);
+      expect(actual).toMatchSnapshot();
     });
-  });
-
-  describe('the `Grid` component', () => {
-    it('should render an `IconCard` for each item in `props.sleepingArrangements`', () => {
-      const wrapper = getSleepingArrangements().find(GridColumn);
-
-      wrapper.forEach((element, index) => {
-        expectComponentToHaveChildren(wrapper.at(index), IconCard);
-      });
-    });
-  });
-
-  describe('each `IconCard` component', () => {
-    it('should get the right props', () => {
-      const wrapper = getSleepingArrangements()
-        .find(IconCard)
-        .first();
-      const { iconName, label } = sleepingArrangements[0];
-
-      expectComponentToHaveProps(wrapper, {
-        isLeftAligned: true,
-        label: label,
-        name: iconName,
-      });
-    });
-  });
-
-  it('should have `displayName` `SleepingArrangements`', () => {
-    expectComponentToHaveDisplayName(
-      SleepingArrangements,
-      'SleepingArrangements'
-    );
   });
 });
