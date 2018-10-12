c323c8c811d0837f65a60ac85587f96adbc53245
Jonny Gwillim <jonnygwillim@gmail.com>
2018-10-12 17:40:33 +0200

    refactor(Summary): use media query responsive wrappers

--- src/components/property-page-widgets/Summary/component.spec.js
+++ src/components/property-page-widgets/Summary/component.spec.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Segment } from 'semantic-ui-react';
-import {
-  expectComponentToBe,
-  expectComponentToHaveChildren,
-  expectComponentToHaveDisplayName,
-} from '@lodgify/enzyme-jest-expect-helpers';
-import { Responsive } from 'semantic-ui-react';
-import { expectComponentToHaveProps } from '@lodgify/enzyme-jest-expect-helpers/lib/expectComponentToHaveProps';
+import { mount, shallow } from 'enzyme';
+import { expectComponentToHaveDisplayName } from '@lodgify/enzyme-jest-expect-helpers';
 
 import { ComponentWithResponsive as Summary } from './component';
 
@@ -23,78 +16,23 @@ const getSummary = () => shallow(<Summary {...props} />);
 const getWrappedSummary = extraProps => {
   const Child = getSummary().prop('as');
 
-  return shallow(<Child isUserOnMobile={false} {...props} {...extraProps} />);
+  return mount(<Child isUserOnMobile={false} {...props} {...extraProps} />);
 };
 
 describe('<Summary/>', () => {
-  it('should be wrapped in a Semantic UI `Responsive` component', () => {
-    const wrapper = getSummary();
+  describe('by default', () => {
+    it('should render the right structure', () => {
+      const actual = getWrappedSummary();
 
-    expectComponentToBe(wrapper, Responsive);
-  });
-
-  describe('the wrapped `Summary` component', () => {
-    it('should render a single `SegmentGroup` component', () => {
-      const wrapper = getWrappedSummary();
-
-      expectComponentToBe(wrapper, Segment.Group);
+      expect(actual).toMatchSnapshot();
     });
   });
 
-  describe('the `Segment.Group`', () => {
-    it('should have the right props', () => {
-      const wrapper = getWrappedSummary();
+  describe('if `props.isUserOnMobile` is true', () => {
+    it('should render the right children', () => {
+      const actual = getWrappedSummary({ isUserOnMobile: true });
 
-      expectComponentToHaveProps(wrapper, { compact: true });
-    });
-
-    it('should render the right children ', () => {
-      const wrapper = getWrappedSummary();
-
-      expectComponentToHaveChildren(wrapper, Segment, Segment.Group);
-    });
-
-    describe('if `props.isUserOnMobile` is true', () => {
-      it('should render the right children', () => {
-        const wrapper = getWrappedSummary({ isUserOnMobile: true })
-          .find(Segment.Group)
-          .at(0);
-
-        expectComponentToHaveChildren(wrapper, Segment.Group);
-      });
-    });
-
-    describe('the second `Segment.Group`', () => {
-      const getSecondSummaryGroup = extraProps =>
-        getWrappedSummary(extraProps)
-          .find(Segment.Group)
-          .at(1);
-
-      it('should have the right props', () => {
-        const wrapper = getSecondSummaryGroup();
-
-        expectComponentToHaveProps(wrapper, { horizontal: true });
-      });
-
-      it('should render the right children', () => {
-        const wrapper = getSecondSummaryGroup();
-
-        expectComponentToHaveChildren(wrapper, Segment, Segment, Segment);
-      });
-
-      describe('if `props.isUserOnMobile` is true', () => {
-        it('should have the right props', () => {
-          const wrapper = getSecondSummaryGroup({ isUserOnMobile: true });
-
-          expectComponentToHaveProps(wrapper, { horizontal: false });
-        });
-
-        it('should render the right children', () => {
-          const wrapper = getSecondSummaryGroup({ isUserOnMobile: true });
-
-          expectComponentToHaveChildren(wrapper, Segment, Segment);
-        });
-      });
+      expect(actual).toMatchSnapshot();
     });
   });
 
