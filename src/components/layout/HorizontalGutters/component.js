e50209aef4c494b1ff4d3ed0efa23aa8d774e4e3
Stephen Kerr <stephen@lodgify.com>
2018-10-24 16:04:09 +0200

    refactor(Container): use specific semantic ui react import

--- src/components/layout/HorizontalGutters/component.js
+++ src/components/layout/HorizontalGutters/component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import Container from 'semantic-ui-react/dist/commonjs/elements/Container';
 
 /**
  * HorizontalGutters wraps child components with responsive
