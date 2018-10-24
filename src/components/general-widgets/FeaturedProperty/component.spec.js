93bdb5795d232f47d1776d9c8296b9b8ba61099a
Stephen Kerr <stephen@lodgify.com>
2018-10-24 11:25:59 +0200

    refactor(FeaturedProperty): use specific semantic ui react import

--- src/components/general-widgets/FeaturedProperty/component.spec.js
+++ src/components/general-widgets/FeaturedProperty/component.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Card, Image } from 'semantic-ui-react';
+import Card from 'semantic-ui-react/dist/commonjs/views/Card';
+import Image from 'semantic-ui-react/dist/commonjs/elements/Image';
 import {
   expectComponentToBe,
   expectComponentToHaveChildren,
