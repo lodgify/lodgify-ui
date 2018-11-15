d5a62d1782f5ca0d730df3eb53ac7ee0ec777d2b
red-runner <pol@lodgify.com>
2018-11-15 18:13:16 +0100

    feat(getRatingData): implement util and unit test

---
+++ src/components/inputs/RatingInput/utils/getRatingData.js
@@ -0,0 +1,7 @@
+/**
+ * @param {Object} reactSyntheticEvent
+ * @param {Object} ratingData
+ * @param {number} ratingData.rating
+ * @return {number}
+ */
+export const getRatingData = (reactSyntheticEvent, { rating }) => rating;
