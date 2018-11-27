afce7cb7331ee51e7bd3bace22a4da8c493bcc10
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-27 16:24:15 +0100

    test(Dropdown): replace unit tests with snapshots

--- src/components/inputs/Dropdown/utils/adaptOptions.spec.js
+++ src/components/inputs/Dropdown/utils/adaptOptions.spec.js
@@ -1,136 +1,47 @@
-import { shallow } from 'enzyme';
-
 import { adaptOptions } from './adaptOptions';
 
 describe('adaptOptions', () => {
-  describe('if no options specify an image', () => {
-    it('should return the `options`', () => {
+  describe('by default', () => {
+    it('should return the right `options` structure', () => {
       const options = [{ some: 'option' }, { no: 'image' }];
       const actual = adaptOptions(options, false);
 
-      expect(actual).toBe(options);
+      expect(actual).toMatchSnapshot();
     });
   });
 
-  describe('if one or more options specify an image', () => {
-    it('should return an array of options', () => {
-      const options = [{ image: 'someImage' }, {}];
+  describe('if one or more options specify an `image`', () => {
+    it('should return the right `options` structure', () => {
+      const options = [
+        {
+          imageUrl: 'someImage',
+          imageSrcSet: 'someSrcSets',
+          imageSizes: 'someSizes',
+        },
+        {},
+      ];
       const actual = adaptOptions(options, true);
 
-      expect(actual).toBeInstanceOf(Array);
+      expect(actual).toMatchSnapshot();
     });
+  });
 
-    describe('the `text` property of each option', () => {
-      it('should be an array', () => {
-        const options = [{ image: 'someImage' }, { text: 'someOtherText' }];
-        const adaptedOptions = adaptOptions(options, true);
-
-        adaptedOptions.forEach(adaptedOption => {
-          const actual = adaptedOption.text;
-
-          expect(actual).toBeInstanceOf(Array);
-        });
-      });
-
-      it('should have an `img` element at index 0', () => {
-        const options = [{ image: 'someImage' }];
-        const adaptedOptions = adaptOptions(options, true);
-        const textIndex0 = adaptedOptions[0].text[0];
-        const element = shallow(textIndex0);
-        const actual = element.is('img');
-
-        expect(actual).toBe(true);
-      });
-
-      it('should pass the right props to the `img`', () => {
-        const options = [{ image: 'someImage', text: 'someText' }];
-        const adaptedOptions = adaptOptions(options, true);
-        const textIndex0 = adaptedOptions[0].text[0];
-        const element = shallow(textIndex0);
-        const actual = element.props();
-
-        expect(actual).toEqual(
-          expect.objectContaining({
-            alt: 'someText',
-            className: 'ui image',
-            src: 'someImage',
-          })
-        );
-      });
-
-      it('should have an `span` element at index 1', () => {
-        const options = [{ image: 'someImage' }];
-        const adaptedOptions = adaptOptions(options, true);
-        const textIndex1 = adaptedOptions[0].text[1];
-        const element = shallow(textIndex1);
-        const actual = element.is('span');
-
-        expect(actual).toBe(true);
-      });
-
-      it('should pass the right props to the `span`', () => {
-        const options = [{ image: 'someImage' }];
-        const adaptedOptions = adaptOptions(options, true);
-        const textIndex1 = adaptedOptions[0].text[1];
-        const element = shallow(textIndex1);
-        const actual = element.props();
-
-        expect(actual).toEqual(
-          expect.objectContaining({
-            className: 'text',
-          })
-        );
-      });
-
-      it('should pass the incoming `text` value to the `span`', () => {
-        const options = [{ image: 'someImage', text: '📝' }];
-        const adaptedOptions = adaptOptions(options, true);
-        const textIndex1 = adaptedOptions[0].text[1];
-        const element = shallow(textIndex1);
-        const actual = element.contains(options[0].text);
-
-        expect(actual).toBe(true);
-      });
-    });
-
-    it('should return the `value` property of each option without modification', () => {
+  describe('if one or more options have an `indent`', () => {
+    it('should return the right `options` structure', () => {
       const options = [
-        { image: 'someImage', value: '💰' },
-        { value: 'self worth' },
-      ];
-      const adaptedOptions = adaptOptions(options, true);
-
-      adaptedOptions.forEach((adaptedOption, index) => {
-        const actual = adaptedOption.value;
-
-        expect(actual).toBe(options[index].value);
-      });
-    });
-
-    it('should not return the `image` property of each option', () => {
-      const options = [{ image: 'someImage' }];
-      const adaptedOptions = adaptOptions(options, true);
-      const actual = adaptedOptions[0].image;
-
-      expect(actual).toBeUndefined();
-    });
-
-    it('should return the right `className` when indent is defined', () => {
-      const options = [
-        { indent: 1, image: 'SRC-1', text: 'bam', value: 'boozled' },
+        {
+          indent: 1,
+          imageUrl: 'SRC-1',
+          imageSrcSet: 'SRC-1-SrcSets',
+          imageSizes: 'SRC-1-someSizes',
+          text: 'bam',
+          value: 'boozled',
+        },
       ];
       const adaptedOptions = adaptOptions(options, false);
       const actual = adaptedOptions[0].className;
 
-      expect(actual).toBe('indent-1');
-    });
-
-    it('should not return `className` indent is undefined', () => {
-      const options = [{ image: 'SRC-2', text: 'C3', value: 'PO' }];
-      const adaptedOptions = adaptOptions(options, false);
-      const actual = adaptedOptions[0].className;
-
-      expect(actual).toBe(undefined);
+      expect(actual).toMatchSnapshot();
     });
   });
 });
