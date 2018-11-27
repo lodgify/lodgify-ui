64aa5d702a061a0dbaa8b9ad689120a85385a655
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-27 16:25:02 +0100

    fix(Availability): update roomOptionsWithImages prop shape

--- src/components/property-page-widgets/Availability/mock-data/options.js
+++ src/components/property-page-widgets/Availability/mock-data/options.js
@@ -2,26 +2,26 @@ export const roomOptionsWithImages = [
   {
     text: 'All rooms',
     value: 'all',
-    image: require('./la-casa-viva.png'),
+    imageUrl: require('./la-casa-viva.png'),
   },
   {
     text: 'La Casa Viva',
     value: 'casaViva',
-    image: require('./la-casa-viva.png'),
+    imageUrl: require('./la-casa-viva.png'),
   },
   {
     text: 'La Casa Muerta',
     value: 'casaMuerta',
-    image: require('./la-casa-muerta.png'),
+    imageUrl: require('./la-casa-muerta.png'),
   },
   {
     text: 'The White Lodge',
     value: 'whiteLodge',
-    image: require('./the-black-lodge.png'),
+    imageUrl: require('./the-black-lodge.png'),
   },
   {
     text: 'The Black Lodge',
     value: 'blackLodge',
-    image: require('./the-white-lodge.png'),
+    imageUrl: require('./the-white-lodge.png'),
   },
 ];
