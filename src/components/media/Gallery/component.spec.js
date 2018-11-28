74bb3402a2f76dd47e0943a04d7fa6e840355d9d
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-28 13:18:34 +0100

    test(Gallery): update specs for new image prop structure

--- src/components/media/Gallery/component.spec.js
+++ src/components/media/Gallery/component.spec.js
@@ -1,146 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import {
-  expectComponentToBe,
-  expectComponentToHaveProps,
-  expectComponentToHaveChildren,
-  expectComponentToHaveDisplayName,
-} from '@lodgify/enzyme-jest-expect-helpers';
-
-import { Modal } from 'elements/Modal';
-import { HorizontalGutters } from 'layout/HorizontalGutters';
-import { Divider } from 'elements/Divider';
-import { Grid } from 'layout/Grid';
-import { GridRow } from 'layout/GridRow';
-import { GridColumn } from 'layout/GridColumn';
-import { Heading } from 'typography/Heading';
-import { ResponsiveImage } from 'media/ResponsiveImage';
+import { expectComponentToHaveDisplayName } from '@lodgify/enzyme-jest-expect-helpers';
 
 import { Component as Gallery } from './component';
 
-const images = [
-  { imageUrl: '💻', label: '🔷' },
-  { imageUrl: '💻', label: '🔷' },
-];
+const images = [{ url: '💻', label: '🔷' }, { url: '💻', label: '🔷' }];
 const trigger = 'someTrigger';
 
 const getGallery = props =>
   shallow(<Gallery images={images} trigger={trigger} {...props} />);
 
 describe('<Gallery />', () => {
-  it('should render a `Modal`', () => {
-    const wrapper = getGallery();
+  describe('by default', () => {
+    it('should render the right structure', () => {
+      const actual = getGallery();
 
-    expectComponentToBe(wrapper, Modal);
-  });
-
-  describe('the `Modal`', () => {
-    it('should get the right props', () => {
-      const wrapper = getGallery();
-
-      expectComponentToHaveProps(wrapper, {
-        isFullscreen: true,
-        trigger,
-      });
-    });
-
-    it('should have the right children', () => {
-      const wrapper = getGallery();
-
-      expectComponentToHaveChildren(wrapper, HorizontalGutters);
+      expect(actual).toMatchSnapshot();
     });
   });
 
-  describe('the `HorizontalGutters`', () => {
-    const getHorizontalGutters = props =>
-      getGallery(props).find(HorizontalGutters);
+  describe('if `props.heading` is defined', () => {
+    it('should render the right structure', () => {
+      const heading = ':hurtrealbad:';
+      const actual = getGallery({ heading });
 
-    it('should have the right children', () => {
-      const wrapper = getHorizontalGutters();
-
-      expectComponentToHaveChildren(wrapper, Divider, Grid, Divider);
-    });
-
-    describe('if `props.heading` is defined', () => {
-      it('shoul have the right children', () => {
-        const heading = ':hurtrealbad:';
-        const wrapper = getHorizontalGutters({ heading });
-
-        expectComponentToHaveChildren(
-          wrapper,
-          Divider,
-          heading,
-          Divider,
-          Grid,
-          Divider
-        );
-      });
-    });
-  });
-
-  describe('the `Grid`', () => {
-    const getGrid = () => getGallery().find(Grid);
-
-    it('should have the right props', () => {
-      const wrapper = getGrid();
-
-      expectComponentToHaveProps(wrapper, {
-        columns: 2,
-        stackable: true,
-      });
-    });
-
-    it('should have the right children', () => {
-      const wrapper = getGrid();
-
-      expectComponentToHaveChildren(wrapper, GridRow);
-    });
-  });
-
-  describe('the `GridRow`', () => {
-    it('should have the right children', () => {
-      const wrapper = getGallery().find(GridRow);
-
-      expectComponentToHaveChildren(wrapper, GridColumn, GridColumn);
-    });
-  });
-
-  describe('each `GridColumn`', () => {
-    it('should have the right children', () => {
-      const wrapper = getGallery()
-        .find(GridColumn)
-        .first();
-
-      expectComponentToHaveChildren(wrapper, Heading, ResponsiveImage, Divider);
-    });
-  });
-
-  describe('each `Heading`', () => {
-    const getFirstHeading = () =>
-      getGallery()
-        .find(Heading)
-        .first();
-
-    it('should havce the right props', () => {
-      const wrapper = getFirstHeading();
-
-      expectComponentToHaveProps(wrapper, { size: 'small' });
-    });
-
-    it('should have the right children', () => {
-      const wrapper = getFirstHeading();
-
-      expectComponentToHaveChildren(wrapper, images[0].label);
-    });
-  });
-
-  describe('each `ResponsiveImage`', () => {
-    it('should havce the right props', () => {
-      const wrapper = getGallery()
-        .find(ResponsiveImage)
-        .first();
-
-      expectComponentToHaveProps(wrapper, { imageUrl: images[0].imageUrl });
+      expect(actual).toMatchSnapshot();
     });
   });
 
