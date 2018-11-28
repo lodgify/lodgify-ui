6c5ed2ca3e47cba3107701ce149e749fe519943e
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-28 13:03:34 +0100

    feat(Pictures): pass correct image prop structure to Thumbnail

--- src/components/property-page-widgets/Pictures/mock-data/pictures.js
+++ src/components/property-page-widgets/Pictures/mock-data/pictures.js
@@ -2,9 +2,17 @@ const imageUrl =
   'https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg?w=1024&mode=max';
 
 export const pictures = [
-  { imageUrl, label: 'Entrance' },
-  { imageUrl, label: 'Kitchen' },
-  { imageUrl, label: 'Living room' },
-  { imageUrl, label: 'Studio' },
-  { imageUrl, label: 'Garden room' },
+  { imageUrl, imageUrlAlternativeText: 'Entrance', label: 'Entrance' },
+  { imageUrl, imageUrlAlternativeText: 'Kitchen', label: 'Kitchen' },
+  {
+    imageUrl,
+    imageUrlAlternativeText: 'Living room',
+    label: 'Living room',
+  },
+  { imageUrl, imageUrlAlternativeText: 'Studio', label: 'Studio' },
+  {
+    imageUrl,
+    imageUrlAlternativeText: 'Garden room',
+    label: 'Garden room',
+  },
 ];
