792c3f7905baed9897421f0a661aef11cf166c7c
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-28 18:15:53 +0100

    test(RoomTypes): update spec to snapshots

--- src/components/property-page-widgets/RoomTypes/component.spec.js
+++ src/components/property-page-widgets/RoomTypes/component.spec.js
@@ -1,33 +1,19 @@
-import React, { Fragment } from 'react';
-import { shallow } from 'enzyme';
-import {
-  expectComponentToBe,
-  expectComponentToHaveChildren,
-  expectComponentToHaveDisplayName,
-} from '@lodgify/enzyme-jest-expect-helpers';
-
-import { RoomType } from 'property-page-widgets/RoomType';
+import React from 'react';
+import { mount } from 'enzyme';
+import { expectComponentToHaveDisplayName } from '@lodgify/enzyme-jest-expect-helpers';
 
 import { Component as RoomTypes } from './component';
 
 const { roomTypes } = require('./mock-data/roomTypes');
 
 const getRoomTypes = otherProps =>
-  shallow(<RoomTypes roomTypes={roomTypes} {...otherProps} />);
+  mount(<RoomTypes roomTypes={roomTypes} {...otherProps} />);
 
-describe('<FeaturedRoomTypes />', () => {
-  it('should have `Fragment` component as a wrapper', () => {
+describe('<RoomTypes />', () => {
+  it('should render the right structure', () => {
     const wrapper = getRoomTypes();
 
-    expectComponentToBe(wrapper, Fragment);
-  });
-
-  describe('the wrapping `Fragment`', () => {
-    it('should render the right children', () => {
-      const wrapper = getRoomTypes();
-
-      expectComponentToHaveChildren(wrapper, RoomType, RoomType);
-    });
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should have `displayName` `RoomTypes`', () => {
