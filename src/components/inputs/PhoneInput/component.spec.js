187a293f9e469f0ad01d30cecfcb07f265a4c467
Marc <mwgdavies@gmail.com>
2018-11-28 11:27:35 +0100

    test(inputs): change to snapshot testing

--- src/components/inputs/PhoneInput/component.spec.js
+++ src/components/inputs/PhoneInput/component.spec.js
@@ -1,55 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import {
-  expectComponentToBe,
-  expectComponentToHaveChildren,
-  expectComponentToHaveDisplayName,
-  expectComponentToHaveProps,
-} from '@lodgify/enzyme-jest-expect-helpers';
-
-import { InputController } from '../InputController';
+import { mount, shallow } from 'enzyme';
+import { expectComponentToHaveDisplayName } from '@lodgify/enzyme-jest-expect-helpers';
 
 import { Component as PhoneInput } from './component';
 
 const getPhoneInput = props => shallow(<PhoneInput {...props} />);
 
-const getInputController = () => getPhoneInput().find('InputController');
-
 describe('<PhoneInput />', () => {
-  it('should render a single `InputController` component', () => {
-    const wrapper = getPhoneInput();
+  it('should render the right structure', () => {
+    const wrapper = mount(<PhoneInput />);
 
-    expectComponentToBe(wrapper, InputController);
-  });
-
-  it('should pass the right `props` to `InputController`', () => {
-    const wrapper = getInputController();
-
-    expectComponentToHaveProps(wrapper, {
-      error: false,
-      icon: expect.any(Object),
-      isValid: false,
-      label: '',
-      name: '',
-      onChange: expect.any(Function),
-    });
-  });
-
-  it('should pass an html `input` as a child to `InputController`', () => {
-    const wrapper = getInputController();
-
-    expectComponentToHaveChildren(wrapper, 'input');
-  });
-
-  describe('the child `input`', () => {
-    it('should get the right props', () => {
-      const wrapper = getInputController().find('input');
-
-      expectComponentToHaveProps(wrapper, {
-        type: 'tel',
-        value: '',
-      });
-    });
+    expect(wrapper).toMatchSnapshot();
   });
 
   describe('Interaction: onChange', () => {
