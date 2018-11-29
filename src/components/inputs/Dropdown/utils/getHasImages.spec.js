d8a6dc0117a42831acfe04c1c92419f76c95727e
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-29 11:43:05 +0100

    test(getHasImages): add unit tests for extra imageSrcSet and imageSizes case

--- src/components/inputs/Dropdown/utils/getHasImages.spec.js
+++ src/components/inputs/Dropdown/utils/getHasImages.spec.js
@@ -11,8 +11,38 @@ describe('getHasImages', () => {
     expect(actual).toBe(true);
   });
 
+  it('should return `true` if object has `imageSizes` and `imageSrcSet` property', () => {
+    const options = [
+      { value: 'firstValue', imageSizes: 'sizes', imageSrcSet: 'srcSet' },
+      { value: 'secondValue' },
+    ];
+    const actual = getHasImages(options);
+
+    expect(actual).toBe(true);
+  });
+
   it('should return `false` if object doesnt have `imageUrl` property', () => {
     const options = [{ value: 'firstYoValue' }, { value: 'secondYoValue' }];
+    const actual = getHasImages(options);
+
+    expect(actual).toBe(false);
+  });
+
+  it('should return `false` if object only has `imageSizes` property', () => {
+    const options = [
+      { value: 'firstYoValue', imageSizes: 'sizes' },
+      { value: 'secondYoValue' },
+    ];
+    const actual = getHasImages(options);
+
+    expect(actual).toBe(false);
+  });
+
+  it('should return `false` if object only has `imageSrcSet` property', () => {
+    const options = [
+      { value: 'firstYoValue', imageSrcSet: 'srcSet' },
+      { value: 'secondYoValue' },
+    ];
     const actual = getHasImages(options);
 
     expect(actual).toBe(false);
