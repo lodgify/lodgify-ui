67a2e19f13fc5fac17463fd1e06c16a0f83f37d8
Jonny Gwillim <jonnygwillim@gmail.com>
2018-11-29 11:42:26 +0100

    feat(getHasImages): add extra case for imageSrcSet and imageSizes

--- src/components/inputs/Dropdown/utils/getHasImages.js
+++ src/components/inputs/Dropdown/utils/getHasImages.js
@@ -3,4 +3,9 @@
  * @return {boolean}
  */
 export const getHasImages = options =>
-  options.some(option => option.hasOwnProperty('imageUrl'));
+  options.some(
+    option =>
+      option.hasOwnProperty('imageUrl') ||
+      (option.hasOwnProperty('imageSrcSet') &&
+        option.hasOwnProperty('imageSizes'))
+  );
