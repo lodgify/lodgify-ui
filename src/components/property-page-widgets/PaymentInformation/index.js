2963a0cbc6ac8ab441da675f39623c749937c1f2
Jonny Gwillim <jonnygwillim@gmail.com>
2018-12-07 17:02:58 +0100

    feat(PaymentInformation): rename component to PolicyAndNotes and remove tax and cleaning related pro

--- src/components/property-page-widgets/PaymentInformation/index.js
+++
@@ -1,1 +0,0 @@
-export { Component as PaymentInformation } from './component';
--- src/components/property-page-widgets/PolicyAndNotes/component.spec.js
+++ src/components/property-page-widgets/PolicyAndNotes/component.spec.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { expectComponentToHaveDisplayName } from '@lodgify/enzyme-jest-expect-helpers';
 
-import { Component as PaymentInformation } from './component';
+import { Component as PolicyAndNotes } from './component';
 
 const paymentScheduleText = `50% due at time of booking. Remaining balance: Due later.`;
 const cancellationPolicyText = `All paid prepayments are non-refundable.`;
-const cleaningCharge = `25$ (USD)`;
-const taxesText = `1%`;
-const taxesDescriptionText = `of total booking value`;
 const damageDepositText = `A refundable damage deposit of 200.00 € (EUR) is due.`;
 const notesText = `
   Livingstone is a modern website template with clean and straight lines. Its special feature is a wide horizontal header photo slideshow in which logo and header navigation nicely blend in.
@@ -27,19 +24,19 @@ const extraNotesText = `
 
 const props = {};
 
-const getPaymentInformation = extraProps =>
-  mount(<PaymentInformation {...props} {...extraProps} />);
+const getPolicyAndNotes = extraProps =>
+  mount(<PolicyAndNotes {...props} {...extraProps} />);
 
-describe('<PaymentInformation />', () => {
+describe('<PolicyAndNotes />', () => {
   it('should have the right structure', () => {
-    const wrapper = getPaymentInformation();
+    const wrapper = getPolicyAndNotes();
 
     expect(wrapper).toMatchSnapshot();
   });
 
   describe('if `props.paymentScheduleText` is passed', () => {
     it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({ paymentScheduleText });
+      const wrapper = getPolicyAndNotes({ paymentScheduleText });
 
       expect(wrapper).toMatchSnapshot();
     });
@@ -47,26 +44,7 @@ describe('<PaymentInformation />', () => {
 
   describe('if `props.cancellationPolicyText` is passed', () => {
     it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({ cancellationPolicyText });
-
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-  describe('if `props.cleaningCharge` is passed', () => {
-    it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({ cleaningCharge });
-
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-
-  describe('if `props.taxesText` and `props.taxesDescriptionText` are passed', () => {
-    it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({
-        taxesDescriptionText,
-        taxesText,
-      });
+      const wrapper = getPolicyAndNotes({ cancellationPolicyText });
 
       expect(wrapper).toMatchSnapshot();
     });
@@ -74,7 +52,7 @@ describe('<PaymentInformation />', () => {
 
   describe('if `props.damageDepositText` is passed', () => {
     it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({
+      const wrapper = getPolicyAndNotes({
         damageDepositText,
       });
 
@@ -84,7 +62,7 @@ describe('<PaymentInformation />', () => {
 
   describe('if `props.notesText` is passed', () => {
     it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({
+      const wrapper = getPolicyAndNotes({
         notesText,
       });
 
@@ -94,7 +72,7 @@ describe('<PaymentInformation />', () => {
 
   describe('if `props.extraNotesText` is passed', () => {
     it('should have the right structure', () => {
-      const wrapper = getPaymentInformation({
+      const wrapper = getPolicyAndNotes({
         extraNotesText,
       });
 
@@ -102,7 +80,7 @@ describe('<PaymentInformation />', () => {
     });
   });
 
-  it('should have `displayName` `PaymentInformation`', () => {
-    expectComponentToHaveDisplayName(PaymentInformation, 'PaymentInformation');
+  it('should have `displayName` `PolicyAndNotes`', () => {
+    expectComponentToHaveDisplayName(PolicyAndNotes, 'PolicyAndNotes');
   });
 });
