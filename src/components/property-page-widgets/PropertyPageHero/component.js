a928cd095222e32c45e8808af8ad37470854d768
Jonny Gwillim <jonnygwillim@gmail.com>
2018-12-10 12:56:35 +0100

    feat(PropertyPageHero): expose responsive image props for header logo

--- src/components/property-page-widgets/PropertyPageHero/component.js
+++ src/components/property-page-widgets/PropertyPageHero/component.js
@@ -21,7 +21,9 @@ import { BOTTOM_OFFSET } from './constants';
 // eslint-disable-next-line jsdoc/require-jsdoc
 const Component = ({
   activeNavigationItemIndex,
+  headerLogoSizes,
   headerLogoSrc,
+  headerLogoSrcSet,
   headerLogoText,
   headerNavigationItems,
   headerPrimaryCTA,
@@ -51,7 +53,9 @@ const Component = ({
       backgroundImageSrcSet={backgroundImageSrcSet}
       backgroundImageUrl={backgroundImageUrl}
       bottomOffset={BOTTOM_OFFSET}
+      headerLogoSizes={headerLogoSizes}
       headerLogoSrc={headerLogoSrc}
+      headerLogoSrcSet={headerLogoSrcSet}
       headerLogoText={headerLogoText}
       headerNavigationItems={headerNavigationItems}
       headerPrimaryCTA={headerPrimaryCTA}
@@ -92,7 +96,9 @@ Component.displayName = 'PropertyPageHero';
 
 Component.defaultProps = {
   activeNavigationItemIndex: null,
+  headerLogoSizes: undefined,
   headerLogoSrc: null,
+  headerLogoSrcSet: undefined,
   headerPrimaryCTA: null,
   propertyName: null,
   ratingNumber: null,
@@ -107,8 +113,12 @@ Component.defaultProps = {
 Component.propTypes = {
   /** The index of the active navigation item. */
   activeNavigationItemIndex: PropTypes.number,
+  /** A list of one or more strings separated by commas indicating a set of source sizes for the header logo. See [the MDN docs for more information](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img). */
+  headerLogoSizes: PropTypes.string,
   /** The src url for the logo in the header. */
   headerLogoSrc: PropTypes.string,
+  /** A list of one or more strings separated by commas indicating a set of possible image sources for the user agent to use for the header logo. See [the MDN docs for more information](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img). */
+  headerLogoSrcSet: PropTypes.string,
   /** The text for the logo in the header. */
   headerLogoText: PropTypes.string.isRequired,
   /** The items for a user to navigate the site. */
