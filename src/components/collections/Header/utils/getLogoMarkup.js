07ba72857338b1513164ddf096ea22aa09fae932
Jonny Gwillim <jonnygwillim@gmail.com>
2018-12-10 12:35:00 +0100

    feat(Header): expose sizes and src props for the logo

--- src/components/collections/Header/utils/getLogoMarkup.js
+++ src/components/collections/Header/utils/getLogoMarkup.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Menu, Image } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
+import { ResponsiveImage } from 'media/ResponsiveImage';
 import { Heading } from 'typography/Heading';
 
 /**
- * @param  {string} logoSrc
  * @param  {string} logoText
+ * @param  {string} logoSrc
+ * @param  {string} logoSizes
+ * @param  {string} logoSrcSet
  * @return {Object}
  */
-export const getLogoMarkup = (logoSrc, logoText) => (
+export const getLogoMarkup = (logoText, logoSrc, logoSizes, logoSrcSet) => (
   <Menu.Item href="/" link>
     {logoSrc ? (
-      <Image alt={logoText} src={logoSrc} />
+      <ResponsiveImage
+        alt={logoText}
+        imageUrl={logoSrc}
+        sizes={logoSizes}
+        srcSet={logoSrcSet}
+      />
     ) : (
       <Heading size="small">{logoText}</Heading>
     )}
