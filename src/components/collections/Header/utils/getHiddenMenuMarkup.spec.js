5a6cd0d80c8f0f934109dfd832778123bbfbbe99
red-runner <pol@lodgify.com>
2018-12-20 13:24:49 +0100

    feat(getHiddenMenuMarkup): pass isHiddenMenuShowingInAllDevices to ShowOn device props

---
+++ src/components/collections/Header/utils/getHiddenMenuMarkup.spec.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { navigationItems } from '../mock-data/navigationItems';
+
+import { getHiddenMenuMarkup } from './getHiddenMenuMarkup';
+
+const logoSizes = 'someLogoSizes';
+const logoSrc = 'someLogoSrc';
+const logoSrcSet = 'someLogoSrcSet';
+const logoText = 'someLogoText';
+const searchBarGuestsOptions = [{ text: '1', value: 1 }];
+const searchBarLocationOptions = [{ text: 'Catania', value: 'catania' }];
+const searchBarGetIsDayBlocked = Function.prototype;
+const searchBarOnSubmit = Function.prototype;
+const searchBarModalHeadingText = 'someHeadingText';
+const searchBarOnChangeInput = Function.prototype;
+const searchBarSearchButton = 'button boy';
+
+const getMarkupAsRenderedComponent = extraProps =>
+  mount(
+    <div>
+      {getHiddenMenuMarkup({
+        dateRangePickerLocaleCode: 'ko',
+        activeNavigationItemIndex: 0,
+        searchBarGetIsDayBlocked,
+        logoSizes,
+        logoSrc,
+        logoSrcSet,
+        logoText,
+        navigationItems,
+        searchBarModalHeadingText,
+        searchBarSearchButton,
+        searchBarOnChangeInput,
+        searchBarOnSubmit,
+        ...extraProps,
+      })}
+    </div>
+  )
+    .children()
+    .first();
+
+describe('getHiddenMenuMarkup', () => {
+  describe('by default', () => {
+    it('should render the right structure', () => {
+      const actual = getMarkupAsRenderedComponent();
+
+      expect(actual).toMatchSnapshot();
+    });
+  });
+
+  describe('if `props.searchBarGuestsOptions` and `props.searchBarLocationOptions` are defined and populated', () => {
+    const getSearchBarMenuItem = () =>
+      getMarkupAsRenderedComponent({
+        searchBarGuestsOptions,
+        searchBarLocationOptions,
+      });
+
+    it('should render the right structure', () => {
+      const actual = getSearchBarMenuItem();
+
+      expect(actual).toMatchSnapshot();
+    });
+  });
+});
