90563d5966ec2d1a7aae4837181d5b78e5840f05
red-runner <pol@lodgify.com>
2018-12-20 15:43:11 +0100

    test(navigationItems): update mock data

--- src/components/collections/Header/mock-data/navigationItems.js
+++ src/components/collections/Header/mock-data/navigationItems.js
@@ -21,4 +21,37 @@ export const navigationItems = [
       },
     ],
   },
+  { text: 'Contact Us', href: '/' },
+  { text: 'What to do', href: '/' },
+];
+
+export const largeNavigationItems = [
+  { text: 'Home', href: '/' },
+  {
+    text: 'All properties',
+    subItems: [
+      {
+        href: '/la-casa-viva',
+        text: 'La Casa Viva',
+      },
+      {
+        href: '/la-casa-muerta',
+        text: 'La Casa Muerta',
+      },
+      {
+        href: '/the-white-lodge',
+        text: 'The White Lodge',
+      },
+      {
+        href: '/the-black-lodge',
+        text: 'The Black Lodge',
+      },
+    ],
+  },
+  { text: 'Contact Us', href: '/' },
+  {
+    text:
+      'What to do when you go out on a Sunday and then somebody attacks you with big floppy fish',
+    href: '/',
+  },
 ];
