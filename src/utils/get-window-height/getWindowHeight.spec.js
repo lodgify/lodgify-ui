bfffbceaaa8190d4deeed9fa62f88edcf05eebd5
red-runner <pol@lodgify.com>
2019-01-10 10:20:26 +0100

    feat(getWindowHeight): implement global util and unit test

---
+++ src/utils/get-window-height/getWindowHeight.spec.js
@@ -0,0 +1,9 @@
+import { getWindowHeight } from './getWindowHeight';
+
+describe('`getWindowHeight`', () => {
+  it('should return the window height', () => {
+    const actual = getWindowHeight();
+
+    expect(actual).toBe(global.window.innerHeight);
+  });
+});
