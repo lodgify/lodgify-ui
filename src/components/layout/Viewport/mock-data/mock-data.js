fc0cf81432e4a169e41c1df05c89937bf5e97c95
red-runner <pol@lodgify.com>
2019-01-24 13:02:50 +0100

    docs(Viewport): implement component documentation

---
+++ src/components/layout/Viewport/mock-data/mock-data.js
@@ -0,0 +1,71 @@
+export const navigationItems = [
+  {
+    text: 'Home',
+  },
+  {
+    text: 'All properties',
+    href: '/all-properties',
+    subItems: [
+      {
+        href: '/la-casa-viva',
+        text: 'La Casa Viva',
+      },
+      {
+        href: '/la-casa-muerta',
+        text: 'La Casa Muerta',
+      },
+      {
+        href: '/the-white-lodge',
+        text: 'The White Lodge',
+      },
+      {
+        href: '/the-black-lodge',
+        text: 'The Black Lodge',
+      },
+    ],
+  },
+  { text: 'Contact Us', href: '/' },
+  { text: 'What to do', href: '/' },
+];
+
+export const images = [
+  {
+    alternativeText: 'Two cats',
+    url: 'https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg',
+    title: 'Two cats',
+  },
+  {
+    alternativeText: 'Two more cats',
+    url: 'https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg',
+    title: 'Two more cats',
+  },
+  {
+    alternativeText: 'Much cats',
+    url: 'https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg',
+    title: 'Much cats',
+  },
+];
+
+export const currencyOptions = [
+  { text: 'EUR', value: 'EUR' },
+  { text: 'USD', value: 'USD' },
+  { text: 'JPY', value: 'JPY' },
+];
+export const languageOptions = [
+  { text: 'English', value: 'en' },
+  { text: 'German', value: 'de' },
+  { text: 'Japanese', value: 'jp' },
+  { text: 'Italian', value: 'it' },
+];
+export const footerNavigationItems = [
+  { href: '/', text: 'Home' },
+  { href: '/contact', text: 'Contact' },
+  { href: '/pool', text: 'Pool' },
+  { href: '/spa', text: 'Spa' },
+  { href: '/tennis-court', text: 'Tennis Court' },
+  { href: '/gym', text: 'Gym' },
+];
+export const socialMediaLinks = [
+  { href: 'https://twitter.com/lodgify', iconName: 'twitter' },
+  { href: 'https://instagram.com/lodgify', iconName: 'instagram' },
+];
