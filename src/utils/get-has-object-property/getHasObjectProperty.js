97573a36dfa3432561c39611bf0b1a9d39699d0c
Marc <marcdavi-es@users.noreply.github.com>
2019-02-07 13:04:06 +0100

    fix(getObjectHasProperty): clean up check

--- src/utils/get-has-object-property/getHasObjectProperty.js
+++ src/utils/get-has-object-property/getHasObjectProperty.js
@@ -4,4 +4,4 @@
  * @return {boolean}
  */
 export const getHasObjectProperty = (options, property) =>
-  !!options ? options.some(option => option.hasOwnProperty(property)) : false;
+  !!options && options.some(option => option.hasOwnProperty(property));
