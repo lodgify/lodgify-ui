c5f827ee29674849a7a134ae76f97a70f804e482
red-runner <pol@lodgify.com>
2019-02-08 13:21:49 +0100

    docs(HTML): implement nested image case

--- src/components/general-widgets/HTML/mock-data/examples.js
+++ src/components/general-widgets/HTML/mock-data/examples.js
@@ -7,6 +7,30 @@ export const headings = `
   </div>
 `;
 
+export const image = `<h2>Nested Image</h2><img src="https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg?w=1024&mode=max"/>`;
+
+export const nestedImage = `
+  <div style="padding: 1rem;">
+    <h3>Nested image in a fixed width wrapper</h3>
+    <div style="width: 200px; box-shadow: inset 0 0 0 1px black; padding: 20px">
+      <img src="https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg?w=1024&mode=max"/>
+      <img src="https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg?w=1024&mode=max"/>
+    </div>
+  </div>`;
+
+export const flexWrappedImage = `
+<div style="padding: 1rem;">
+  <h3>Nested image in a flex wrapper</h3>
+  <div style="display: flex; flex-direction: row; padding: 20px; box-shadow: inset 0 0 0 1px black;">
+    <div style="margin: 5px;">
+      <img src="https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg?w=1024&mode=max"/>
+    </div>
+    <div style="margin: 5px;">
+      <img src="https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg?w=1024&mode=max"/>
+    </div>
+  </div>
+</div>`;
+
 export const malformedTable = `<TABLE><tr><td>Malformed Table 😱😱😱😱😱😱</tr></TABL>`;
 
 export const malformedQuotes = `<UL style="list-style-type: none;margin: 0;padding: 0;">
