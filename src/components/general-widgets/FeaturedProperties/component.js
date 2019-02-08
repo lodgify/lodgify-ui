9593a6daf32165cd132c0075ae5ed1d9e758b59c
Jonny Gwillim <jonnygwillim@gmail.com>
2019-02-08 16:41:57 +0100

    feat(FeaturedProperties): expose isShowingPlaceholder prop

--- src/components/general-widgets/FeaturedProperties/component.js
+++ src/components/general-widgets/FeaturedProperties/component.js
@@ -7,39 +7,59 @@ import { Heading } from 'typography/Heading';
 import { FeaturedProperty } from 'general-widgets/FeaturedProperty';
 import { buildKeyFromStrings } from 'utils/build-key-from-strings';
 
+import { PLACEHOLDERS } from './constants';
+
 /**
  * The standard widget for displaying a list of featured properties.
  */
 // eslint-disable-next-line jsdoc/require-jsdoc
-export const Component = ({ featuredProperties, headingText }) => (
-  <Grid>
-    {headingText && (
-      <GridColumn width={12}>
-        <Heading>{headingText}</Heading>
-      </GridColumn>
-    )}
-    {featuredProperties.map((featuredProperty, index) => (
-      <GridColumn
-        computer={4}
-        key={buildKeyFromStrings(featuredProperty.propertyName, index)}
-        mobile={12}
-        tablet={6}
-      >
-        <FeaturedProperty {...featuredProperty} />
-      </GridColumn>
-    ))}
-  </Grid>
-);
+export const Component = ({
+  featuredProperties,
+  headingText,
+  isShowingPlaceholder,
+}) => {
+  const featuredPropertiesToMap =
+    isShowingPlaceholder && featuredProperties.length === 0
+      ? PLACEHOLDERS
+      : featuredProperties;
+
+  return (
+    <Grid>
+      {headingText && (
+        <GridColumn width={12}>
+          <Heading>{headingText}</Heading>
+        </GridColumn>
+      )}
+      {featuredPropertiesToMap.map((featuredProperty, index) => (
+        <GridColumn
+          computer={4}
+          key={buildKeyFromStrings(featuredProperty.propertyName, index)}
+          mobile={12}
+          tablet={6}
+        >
+          <FeaturedProperty
+            isShowingPlaceholder={isShowingPlaceholder}
+            {...featuredProperty}
+          />
+        </GridColumn>
+      ))}
+    </Grid>
+  );
+};
 
 Component.displayName = 'FeaturedProperties';
 
 Component.defaultProps = {
+  featuredProperties: [],
   headingText: null,
+  isShowingPlaceholder: false,
 };
 
 Component.propTypes = {
   /** An array of [`FeaturedProperty`](#/FeaturedProperty) props objects. */
-  featuredProperties: PropTypes.arrayOf(PropTypes.object).isRequired,
+  featuredProperties: PropTypes.arrayOf(PropTypes.object),
   /** The text to display as a heading at the top of the widget. */
   headingText: PropTypes.string,
+  /** Is the component showing placeholders to reserve space for content which will appear. */
+  isShowingPlaceholder: PropTypes.bool,
 };
---
+++ src/components/general-widgets/FeaturedProperties/constants.js
@@ -0,0 +1,17 @@
+const PLACEHOLDER_DATA = {
+  bedroomsNumber: 0,
+  guestsNumber: 0,
+  imageUrl: '',
+  locationName: '',
+  nightPrice: '',
+  propertyName: '',
+  propertyType: '',
+  propertyUrl: '',
+  ratingNumber: 0,
+};
+
+const NUMBER_OF_PLACEHOLDERS = 3;
+
+export const PLACEHOLDERS = Array(NUMBER_OF_PLACEHOLDERS).fill(
+  PLACEHOLDER_DATA
+);
