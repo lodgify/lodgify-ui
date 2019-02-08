78aad69b7e7353cc543093e45d84366c052bd00b
Jonny Gwillim <jonnygwillim@gmail.com>
2019-02-08 10:15:23 +0100

    feat(FeaturedRoomTypes): render 3 placeholders when no featuredRoomTypes

--- src/components/general-widgets/FeaturedRoomTypes/component.js
+++ src/components/general-widgets/FeaturedRoomTypes/component.js
@@ -7,6 +7,8 @@ import { Heading } from 'typography/Heading';
 import { FeaturedRoomType } from 'general-widgets/FeaturedRoomType';
 import { buildKeyFromStrings } from 'utils/build-key-from-strings';
 
+import { PLACEHOLDERS } from './constants';
+
 /**
  * The standard widget for displaying a list of featured rooms.
  */
@@ -15,39 +17,47 @@ export const Component = ({
   featuredRoomTypes,
   headingText,
   isShowingPlaceholder,
-}) => (
-  <Grid>
-    {headingText && (
-      <GridColumn width={12}>
-        <Heading>{headingText}</Heading>
-      </GridColumn>
-    )}
-    {featuredRoomTypes.map((featuredRoomType, index) => (
-      <GridColumn
-        computer={4}
-        key={buildKeyFromStrings(featuredRoomType.roomTypeName, index)}
-        mobile={12}
-        tablet={6}
-      >
-        <FeaturedRoomType
-          {...featuredRoomType}
-          isShowingPlaceholder={isShowingPlaceholder}
-        />
-      </GridColumn>
-    ))}
-  </Grid>
-);
+}) => {
+  const featuredRoomTypesToMap =
+    isShowingPlaceholder && featuredRoomTypes.length === 0
+      ? PLACEHOLDERS
+      : featuredRoomTypes;
+
+  return (
+    <Grid>
+      {headingText && (
+        <GridColumn width={12}>
+          <Heading>{headingText}</Heading>
+        </GridColumn>
+      )}
+      {featuredRoomTypesToMap.map((featuredRoomType, index) => (
+        <GridColumn
+          computer={4}
+          key={buildKeyFromStrings(featuredRoomType.roomTypeName, index)}
+          mobile={12}
+          tablet={6}
+        >
+          <FeaturedRoomType
+            isShowingPlaceholder={isShowingPlaceholder}
+            {...featuredRoomType}
+          />
+        </GridColumn>
+      ))}
+    </Grid>
+  );
+};
 
 Component.displayName = 'FeaturedRoomTypes';
 
 Component.defaultProps = {
   headingText: null,
   isShowingPlaceholder: false,
+  featuredRoomTypes: [],
 };
 
 Component.propTypes = {
   /** An array of [`FeaturedRoomType`](#/FeaturedRoomType) props objects. */
-  featuredRoomTypes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  featuredRoomTypes: PropTypes.arrayOf(PropTypes.object),
   /** The text to display as a heading at the top of the widget. */
   headingText: PropTypes.string,
   /** Is the component showing placeholders to reserve space for content which will appear. */
