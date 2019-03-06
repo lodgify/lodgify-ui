c2172fb137802ff1ec99359675f04180fbb1bba5
Marc <mwgdavies@gmail.com>
2019-03-06 16:43:42 +0100

    refactor(Pictures): fit with new Gallery api

--- src/components/property-page-widgets/Pictures/component.js
+++ src/components/property-page-widgets/Pictures/component.js
@@ -63,7 +63,7 @@ export const Component = ({
     </GridRow>
     <GridColumn width={12}>
       <Gallery
-        heading={getGalleryHeadingMarkup(propertyName, ratingNumber)}
+        headingText={getGalleryHeadingMarkup(propertyName, ratingNumber)}
         images={galleryImages}
         trigger={<Link>{linkText}</Link>}
       />
@@ -82,8 +82,37 @@ Component.defaultProps = {
 };
 
 Component.propTypes = {
-  /** The images to display in the gallery modal. See [the `ResponsiveImage` component for valid props](#/Media/ResponsiveImage).  */
-  galleryImages: PropTypes.arrayOf(PropTypes.object).isRequired,
+  /** The images to display in the gallery modal. */
+  galleryImages: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.shape({
+        /** The text to display above an isolated category of images. */
+        categoryText: PropTypes.string,
+      }),
+      PropTypes.shape({
+        /** Alternative text to show if the image can't be loaded by the browser */
+        alternativeText: PropTypes.string,
+        /** The natural height of the image. */
+        imageHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        /** The label text for the when the image is not found. */
+        imageNotFoundLabelText: PropTypes.string,
+        /** Title of the image to show when hovering it on desktop browsers */
+        imageTitle: PropTypes.string,
+        /** URL pointing to the image to display. */
+        imageUrl: PropTypes.string.isRequired,
+        /** The natural width of the image. */
+        imageWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        /** A visible label for the image. */
+        label: PropTypes.string.isRequired,
+        /** URL pointing to the placeholder image to display. */
+        placeholderImageUrl: PropTypes.string,
+        /** A list of one or more strings separated by commas indicating a set of source sizes. See [the MDN docs for more information](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img). */
+        sizes: PropTypes.string,
+        /** A list of one or more strings separated by commas indicating a set of possible image sources for the user agent to use. See [the MDN docs for more information](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img). */
+        srcSet: PropTypes.string,
+      }),
+    ])
+  ).isRequired,
   /** The text to display as a heading at the top of the widget. */
   headingText: PropTypes.string,
   /** The text to display on the link at the bottom of the widget. */
