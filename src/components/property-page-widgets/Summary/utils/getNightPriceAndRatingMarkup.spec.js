909e02bdec83902a64698b5483c782490466efbf
Jonny Gwillim <jonnygwillim@gmail.com>
2019-03-07 13:01:49 +0100

    test(Summary): add unit tests

--- src/components/property-page-widgets/Summary/utils/getNightPriceAndRatingMarkup.spec.js
+++ src/components/property-page-widgets/Summary/utils/getNightPriceAndRatingMarkup.spec.js
@@ -5,10 +5,26 @@ import { getNightPriceAndRatingMarkup } from './getNightPriceAndRatingMarkup';
 
 const nightPrice = '280$';
 
-const getMarkupAsRenderedComponent = ratingNumber =>
-  mount(<div>{getNightPriceAndRatingMarkup(ratingNumber, nightPrice)}</div>);
+const getMarkupAsRenderedComponent = (ratingNumber, isShowingPlaceholder) =>
+  mount(
+    <div>
+      {getNightPriceAndRatingMarkup(
+        ratingNumber,
+        nightPrice,
+        isShowingPlaceholder
+      )}
+    </div>
+  );
 
 describe('getNightPriceAndRatingMarkup', () => {
+  describe('if `isShowingPlaceholder` === true', () => {
+    it('should return the correct structure', () => {
+      const actual = getMarkupAsRenderedComponent(undefined, true);
+
+      expect(actual).toMatchSnapshot();
+    });
+  });
+
   describe('if `ratingNumber` is not `0`', () => {
     it('should return the right markup', () => {
       const ratingNumber = 1;
