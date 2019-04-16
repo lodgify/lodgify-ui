0b1334aa84660480de1b2550e57da15dfd0476f5
Jonny Gwillim <jonnygwillim@gmail.com>
2019-04-16 10:37:27 +0200

    feat(OwnerSignUp): expose privacy consent prop

--- src/components/general-widgets/OwnerSignUp/component.spec.js
+++ src/components/general-widgets/OwnerSignUp/component.spec.js
@@ -33,6 +33,16 @@ describe('<OwnerSignUp />', () => {
     });
   });
 
+  describe('if `isPrivacyConsentRequired` is `truthy`', () => {
+    it('should render the right structure', () => {
+      const wrapper = getOwnerSignUp({
+        isPrivacyConsentRequired: true,
+      });
+
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
   it('should have displayName `OwnerSignUp`', () => {
     expectComponentToHaveDisplayName(OwnerSignUp, 'OwnerSignUp');
   });
