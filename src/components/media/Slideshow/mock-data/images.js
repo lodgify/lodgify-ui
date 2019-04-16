87ed23838808fa119ba196360218df4012df1037
red-runner <pol@lodgify.com>
2019-04-16 11:41:28 +0200

    test(Slideshow): update unit tests with load and resize handlers

--- src/components/media/Slideshow/mock-data/images.js
+++ src/components/media/Slideshow/mock-data/images.js
@@ -1,6 +1,8 @@
 const url =
   'https://li3.cdbcdn.com/oh/522a12d9-ab51-4635-94c1-42536f286e4d.jpg';
 
+const brokenImageUrl = 'https://glue.cdbcdn.com/bicep/bicep.jpg';
+
 export const images = [
   {
     alternativeText: 'Two cats',
@@ -37,5 +39,17 @@ export const images = [
         ${url}?w=800&h=800&mode=max 768w,
         ${url}?w=1024&h=1000&mode=max 1024w`,
     title: 'No dogs',
+  },
+];
+
+export const brokenImages = [
+  {
+    alternativeText: 'Much cats',
+    url: brokenImageUrl,
+    sizes: '100w',
+    srcSet: `${url}?w=400&h=400&mode=max 320w,
+        ${url}?w=800&h=800&mode=max 768w,
+        ${url}?w=1024&h=1000&mode=max 1024w`,
+    title: 'Much cats',
   },
 ];
