a811321e84843f9410577ea736917537610edec1
red-runner <pol@lodgify.com>
2019-04-25 13:47:29 +0200

    feat(renderBlockPlaceholder): implement util and unit test

---
+++ src/components/media/Slideshow/utils/renderBlockPlaceholder.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { BlockPlaceholder } from 'elements/BlockPlaceholder';
+
+/**
+ * @return {Object}
+ */
+export const renderBlockPlaceholder = () => (
+  <BlockPlaceholder isFluid isRectangular />
+);
