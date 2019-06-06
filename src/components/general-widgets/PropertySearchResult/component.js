3957f8fd82237947974516f161ae93dfc631e892
Marc <mwgdavies@gmail.com>
2019-06-06 12:16:08 +0100

    feat(PropertySearchResult): create dummy component

---
+++ src/components/general-widgets/PropertySearchResult/component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+/**
+ * JUST A DUMMY
+ */
+// eslint-disable-next-line jsdoc/require-jsdoc
+export const Component = () => <div>Property search result</div>;
+
+Component.displayName = 'PropertySearchResult';
---
+++ src/components/general-widgets/PropertySearchResult/index.js
@@ -0,0 +1,1 @@
+export { Component as PropertySearchResult } from './component';
