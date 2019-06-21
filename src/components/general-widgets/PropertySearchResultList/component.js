c14decb7ada8d9409179990b48a611750f7a8199
Jonny Gwillim <jonnygwillim@gmail.com>
2019-06-21 15:26:08 +0200

    feat(PropertySearchResultList): implement pagination for 12 or more items

--- src/components/general-widgets/PropertySearchResultList/component.js
+++ src/components/general-widgets/PropertySearchResultList/component.js
@@ -1,9 +1,11 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import { Divider } from 'elements/Divider';
 import { TextPlaceholder } from 'elements/TextPlaceholder';
+import { Pagination } from 'elements/Pagination';
 import { Heading } from 'typography/Heading';
+import { Paragraph } from 'typography/Paragraph';
 import { size } from 'utils/size';
 import { Dropdown } from 'inputs/Dropdown';
 import { FlexContainer } from 'layout/FlexContainer';
@@ -11,66 +13,125 @@ import { PropertySearchResult } from 'general-widgets/PropertySearchResult';
 import { buildKeyFromStrings } from 'utils/build-key-from-strings';
 import { RESULTS } from 'utils/default-strings';
 
-import { PLACEHOLDERS } from './constants';
+import { PLACEHOLDERS, NUMBER_OF_PROPERTIES_PER_PAGE } from './constants';
+import { getNumberOfPages } from './utils/getNumberOfPages';
+import { getPropertySearchResultsToDisplay } from './utils/getPropertySearchResultsToDisplay';
+import { getFirstPropertyPositionOfActivePage } from './utils/getFirstPropertyPositionOfActivePage';
+import { getLastPropertyPositionOfActivePage } from './utils/getLastPropertyPositionOfActivePage';
+import { getShowingResultsText } from './utils/getShowingResultsText';
 
 /**
  * The standard widget for displaying a list of property search results.
  */
 // eslint-disable-next-line jsdoc/require-jsdoc
-export const Component = ({
-  dropdownLabel,
-  dropdownOnChange,
-  dropdownOptions,
-  dropdownValue,
-  isShowingPlaceholder,
-  propertySearchResults,
-  resultsCountText,
-}) =>
-  isShowingPlaceholder ? (
-    <Fragment>
-      <Divider size="small" />
-      <TextPlaceholder length="short" />
-      <Divider size="small" />
-      <FlexContainer flexWrap="wrap" justifyContent="center">
-        {PLACEHOLDERS.map((propertySearchResult, index) => (
-          <PropertySearchResult
-            isShowingPlaceholder
-            key={buildKeyFromStrings(
-              propertySearchResult.placeholderName,
-              index
-            )}
-            {...propertySearchResult}
-          />
-        ))}
-      </FlexContainer>
-    </Fragment>
-  ) : (
-    <Fragment>
-      <FlexContainer alignItems="center" justifyContent="space-between">
-        <Heading size="small">
-          {`${propertySearchResults.length} ${resultsCountText}`}
-        </Heading>
-        {!!size(dropdownOptions) && (
-          <Dropdown
-            isCompact
-            label={dropdownLabel}
-            onChange={dropdownOnChange}
-            options={dropdownOptions}
-            value={dropdownValue}
-          />
+export class Component extends PureComponent {
+  state = {
+    activePage: 1,
+    propertySearchResultsToDisplay: getPropertySearchResultsToDisplay(
+      1,
+      this.props.propertySearchResults
+    ),
+  };
+
+  handleOnPageChange = (event, { activePage }) => {
+    this.setState({
+      activePage,
+      propertySearchResultsToDisplay: getPropertySearchResultsToDisplay(
+        activePage,
+        this.props.propertySearchResults
+      ),
+    });
+  };
+
+  render = () => {
+    const {
+      dropdownLabel,
+      dropdownOnChange,
+      dropdownOptions,
+      dropdownValue,
+      isShowingPlaceholder,
+      propertySearchResults,
+      resultsCountText,
+      renderShowingResultsText,
+    } = this.props;
+
+    const { length: propertySearchResultsLength } = propertySearchResults;
+
+    const { activePage, propertySearchResultsToDisplay } = this.state;
+
+    return isShowingPlaceholder ? (
+      <Fragment>
+        <Divider size="small" />
+        <TextPlaceholder length="short" />
+        <Divider size="small" />
+        <FlexContainer flexWrap="wrap" justifyContent="center">
+          {PLACEHOLDERS.map((propertySearchResult, index) => (
+            <PropertySearchResult
+              isShowingPlaceholder
+              key={buildKeyFromStrings(
+                propertySearchResult.placeholderName,
+                index
+              )}
+              {...propertySearchResult}
+            />
+          ))}
+        </FlexContainer>
+      </Fragment>
+    ) : (
+      <Fragment>
+        <FlexContainer alignItems="center" justifyContent="space-between">
+          <Heading size="small">
+            {`${propertySearchResultsLength} ${resultsCountText}`}
+          </Heading>
+          {!!size(dropdownOptions) && (
+            <Dropdown
+              isCompact
+              label={dropdownLabel}
+              onChange={dropdownOnChange}
+              options={dropdownOptions}
+              value={dropdownValue}
+            />
+          )}
+        </FlexContainer>
+        <Divider size="small" />
+        <FlexContainer flexWrap="wrap" justifyContent="center">
+          {propertySearchResultsToDisplay.map((propertySearchResult, index) => (
+            <PropertySearchResult
+              key={buildKeyFromStrings(
+                propertySearchResult.propertyName,
+                index
+              )}
+              {...propertySearchResult}
+            />
+          ))}
+        </FlexContainer>
+        {propertySearchResultsLength > NUMBER_OF_PROPERTIES_PER_PAGE && (
+          <FlexContainer
+            alignItems="center"
+            flexDirection="column"
+            flexWrap="wrap"
+          >
+            <Pagination
+              isShowingPageNumbers
+              onPageChange={this.handleOnPageChange}
+              totalPages={getNumberOfPages(propertySearchResultsLength)}
+            />
+            <Paragraph>
+              {renderShowingResultsText(
+                getFirstPropertyPositionOfActivePage(activePage),
+                getLastPropertyPositionOfActivePage(
+                  activePage,
+                  propertySearchResultsLength
+                ),
+                propertySearchResultsLength
+              )}
+            </Paragraph>
+          </FlexContainer>
         )}
-      </FlexContainer>
-      <Divider size="small" />
-      <FlexContainer flexWrap="wrap" justifyContent="center">
-        {propertySearchResults.map((propertySearchResult, index) => (
-          <PropertySearchResult
-            key={buildKeyFromStrings(propertySearchResult.propertyName, index)}
-            {...propertySearchResult}
-          />
-        ))}
-      </FlexContainer>
-    </Fragment>
-  );
+      </Fragment>
+    );
+  };
+}
 
 Component.displayName = 'PropertySearchResultList';
 
@@ -82,6 +143,7 @@ Component.defaultProps = {
   propertySearchResults: [],
   resultsCountText: RESULTS,
   isShowingPlaceholder: false,
+  renderShowingResultsText: getShowingResultsText,
 };
 
 Component.propTypes = {
@@ -116,5 +178,12 @@ Component.propTypes = {
   /** An array of [`PropertySearchResult`](#/PropertySearchResult) props objects. */
   propertySearchResults: PropTypes.arrayOf(PropTypes.object),
   /** The text to display alongside the results count. */
+  renderShowingResultsText: PropTypes.func,
+  /**
+   * Function called for creating text that shows how many items are showing.
+   * @param  {number} activePageFirstItemPosition
+   * @param  {number} activePageLastItemPosition
+   * @param  {number} numberOfProperties
+   */
   resultsCountText: PropTypes.string,
 };
