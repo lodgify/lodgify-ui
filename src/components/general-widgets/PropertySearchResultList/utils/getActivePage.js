d605736287fea43c431fe5afb4f515ae508cfba9
Marc <mwgdavies@gmail.com>
2019-08-08 11:45:16 +0200

    fix(PropertySearchresultList): make the controlled pagination work (again)

--- src/components/general-widgets/PropertySearchResultList/utils/getActivePage.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param  {number|string} propsActivePage
- * @param  {number|string} stateActivePage
- * @return {number|string}
- */
-export const getActivePage = (propsActivePage, stateActivePage) =>
-  typeof propsActivePage === 'undefined' ? stateActivePage : propsActivePage;
