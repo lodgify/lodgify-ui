ed5088f7ffe642739e02af30ee4e3fc6c21912a8
Jonny Gwillim <jonnygwillim@gmail.com>
2019-08-13 12:00:51 +0200

    feat(Message): expose isTextAlignedCenter prop

--- src/components/elements/Message/component.js
+++ src/components/elements/Message/component.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Message } from 'semantic-ui-react';
 
 /**
  * A message displays information that explains nearby content.
  */
 // eslint-disable-next-line jsdoc/require-jsdoc
-export const Component = ({ children }) => (
-  <Message className="padded">{children}</Message>
+export const Component = ({ children, isTextAlignedCenter }) => (
+  <Message
+    className={classNames('padded', {
+      'text-aligned-center': isTextAlignedCenter,
+    })}
+  >
+    {children}
+  </Message>
 );
 
 Component.displayName = 'Message';
 
 Component.defaultProps = {
   children: null,
+  isTextAlignedCenter: false,
 };
 
 Component.propTypes = {
   /** The children to show in the message. */
   children: PropTypes.node,
+  /** Is the message text aligned to the center. */
+  isTextAlignedCenter: PropTypes.bool,
 };
