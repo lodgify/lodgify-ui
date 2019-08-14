1ff4e44ab7177043bd39fe3d5132b41bbb219341
Marc <mwgdavies@gmail.com>
2019-08-14 17:28:19 +0200

    docs(GoogleMapReact): add detail to JSDoc for bounds functions

--- src/utils/google-map-react/components/GoogleMapReact/utils/adaptENSWtoNESW.js
+++ src/utils/google-map-react/components/GoogleMapReact/utils/adaptENSWtoNESW.js
@@ -1,5 +1,9 @@
 /**
- * @param  {Object} bounds
+ * @param  {Object}  bounds
+ * @param  {number}  bounds.east
+ * @param  {number}  bounds.north
+ * @param  {number}  bounds.south
+ * @param  {number}  bounds.west
  * @return {Object}
  */
 export const adaptENSWtoNESW = ({ east, north, south, west }) => ({
