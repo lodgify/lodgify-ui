b984ab0f59fcae7e8e0b14d5b0f8b218d9c40323
red-runner <pol@lodgify.com>
2019-12-11 11:39:06 +0100

    feat(useScroll): add custom hook

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { debounce } from 'debounce';
+
+const SCROLL_DEBOUNCE_INTERVAL = 100;
+
+export const useScroll = (callback, initialValue) => {
+  const [result, setResult] = useState(initialValue);
+  const handleScroll = debounce(event => {
+    setResult(callback(event));
+  }, SCROLL_DEBOUNCE_INTERVAL);
+
+  useEffect(() => {
+    global.document.addEventListener('scroll', handleScroll);
+    return () => {
+      global.document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return result;
+};
