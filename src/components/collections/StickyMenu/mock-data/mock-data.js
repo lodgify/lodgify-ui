b1249caf887615ef0f5ab4a7d0270d27021975f8
red-runner <pol@lodgify.com>
2019-12-11 11:39:53 +0100

    feat(StickyMenu): implement StickyMenu

---
+++ src/components/collections/StickyMenu/mock-data/mock-data.js
@@ -0,0 +1,29 @@
+export const menuItems = [
+  {
+    text: 'availability',
+    location: '#availability',
+    active: true,
+  },
+  {
+    text: 'rates',
+    location: '#rates',
+  },
+  {
+    text: 'reviews',
+    location: '#reviews',
+  },
+  {
+    text: 'description',
+    location: '#description',
+  },
+];
+
+export const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '500px',
+  height: '400px',
+  width: '100%',
+  backgroundColor: '#fdb2b2',
+};
